031e6de8013dca6057a0308294712d2be4e90969
Pablo Ruz <pruz@gux.cl>
2019-10-22 16:25:29 -0300

    Componente Login

---
+++ src/Pages/Login.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Container, Row, Col} from 'reactstrap';
+
+class Login extends Component {
+	render() {
+		return (
+            <>
+                <Container>
+                    <Row>
+                        <Col>
+                            Esto es el login
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        )
+	}
+}
+
+export default Login;
