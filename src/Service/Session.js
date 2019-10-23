e47e9410744ecd39f02300083ef75e3efae23e6d
Pablo Ruz <pruz@gux.cl>
2019-10-23 09:11:56 -0300

    Archivo session con parametros de prueba

---
+++ src/Service/Session.js
@@ -0,0 +1,25 @@
+class Session {
+	openTmpPopUp = (token, rut, usuario, correo) => {
+		localStorage.setItem('token', token)
+		localStorage.setItem('rut', rut)
+		localStorage.setItem('usuario', usuario)
+		localStorage.setItem('correo', correo)
+	}
+	isAuth= () => {
+		let isAuth = ("token" in localStorage)
+		return isAuth
+		 
+	}
+	getInfoUser = () => {
+		return {
+			token:localStorage.getItem("token"),
+			rut:localStorage.getItem("rut"),
+			correo:localStorage.getItem("correo"),
+			usuario:localStorage.getItem("usuario"),
+		}
+	}
+	close = () => {
+		localStorage.clear()		
+	}
+}
+export default new Session()
