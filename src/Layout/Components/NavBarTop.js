6a5f5113e554ebabbc9636542a81c76a481ab0f4
Pablo Ruz <pruz@gux.cl>
2019-10-23 12:26:53 -0300

    Se adapta estructura de navbartop

--- src/Layout/Components/NavBarTop.js
+++ src/Layout/Components/NavBarTop.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Row, Col} from 'reactstrap';
-import { Link } from "react-router-dom"
 
 export default class NavBarTop extends React.Component {
   constructor(props) {
@@ -13,11 +12,8 @@ export default class NavBarTop extends React.Component {
     return (
       <div className="NavBarTop">
         <Row>
-          <Col xs={6} md={6} className="text-left">
+          <Col xs={12} md={12} className="text-left">
             <p className="navBarTextTitle">{this.props.nameNavBarTop}</p>
-          </Col>
-          <Col xs={6} md={6} className="text-right">
-            <Link to="/" className="volver">Volver</Link>
           </Col>
         </Row>
       </div>
