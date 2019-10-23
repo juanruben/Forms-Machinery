d5378b6e2cb317bc7fa01313b8617425ee7bb1ec
Pablo Ruz <pruz@gux.cl>
2019-10-23 11:26:46 -0300

    Nueva ruta recuperar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from './Pages/Login';
+import Recuperar from './Pages/Recuperar';
 import Home from './Pages/Home';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
             <Switch> 
               <Route exact path="/" component={Login}  />
               <Route exact path="/Home" component={Home}  />
+              <Route exact path="/Recuperar" component={Recuperar}  />
             </Switch>
         </Router>
     )
