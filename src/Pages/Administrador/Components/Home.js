2e6536a69ef99b7dd13fbf3d25ebc9d80c1e7be8
Pablo Ruz <pruz@gux.cl>
2019-10-23 12:28:31 -0300

    Se agrega archivo inicial administrador

---
+++ src/Pages/Administrador/Components/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Container, Row, Col} from 'reactstrap';
+
+import Layout from '../../../Layout';
+
+class Home extends Component {
+	render() {
+		return (
+            <>
+                <Layout name="Esto es el topheader">
+                    <Container>
+                        <Row>
+                            <Col>
+                                Esto es el Home del administrador
+                            </Col>
+                        </Row>
+                    </Container>
+                </Layout>
+            </>
+        )
+	}
+}
+
+export default Home;
