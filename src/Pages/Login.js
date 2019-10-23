54ed6d21565ae85dbd66a8e79653e349a61c368e
Pablo Ruz <pruz@gux.cl>
2019-10-23 11:26:32 -0300

    Page estructura Login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Row, Col, Form, FormGroup, Button, Alert} from 'reactstrap';
 
 
-//import Logo from './../Assets/Images/Logo.png';
+import Logo from './../Assets/Images/Logo.png';
 
 class Login extends Component {
     constructor(props){
@@ -19,35 +20,41 @@ class Login extends Component {
             <>
                 <Container>
                     <Row>
-                        <Col>
-                            <div className="content-logo">
-                                {/*<img src={Logo} alt="" />*/}
+                        <Col  md={{ size: 6, offset: 3 }}>
+                            <div className="container-login box">
+                                <div className="container__logo">
+                                    <img src={Logo} alt="" />
+                                </div>
+                                <Form>
+                                    <FormGroup row>
+                                        <Col sm={12}>
+                                            <input type="text" name="usuario" id="usuario" onChange={this.handleInputChange} value={this.state.usuario || ''} placeholder="Usuario" className="form-control" />
+                                        </Col>
+                                    </FormGroup>
+                                    <FormGroup row>
+                                        <Col sm={12}>
+                                            <input type="password" name="password" id="password" onChange={this.handleInputChange} value={this.state.password || ''} placeholder="Password" className="form-control" />
+                                        </Col>
+                                    </FormGroup>
+                                    <FormGroup row>
+                                        <Col sm={12} className="text-center"> 
+                                            {this.state.txtMsj !== "" ? <Alert className={'alert '+this.state.AlertClass}>{this.state.txtMsj}</Alert> : "" }
+                                        </Col>
+                                    </FormGroup>
+                                    <FormGroup row>
+                                        <Col sm={12} className="text-center">
+                                            <Button onClick={this.setForm} className="btn-orange">Entrar</Button>
+                                        </Col>
+                                    </FormGroup>
+                                    <FormGroup row>
+                                        <Col sm={12} className="text-center">
+                                            <Link to='/recuperar' className="link">Olvidó su contraseña</Link>
+                                        </Col>
+                                    </FormGroup>
+                                </Form>
                             </div>
                         </Col>
                     </Row>
-                    <Form>
-                    
-                        <FormGroup row>
-                            <Col sm={12}>
-                                <input type="text" name="usuario" id="usuario" onChange={this.handleInputChange} value={this.state.usuario || ''} placeholder="Usuario" className="form-control" />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Col sm={12}>
-                                <input type="password" name="password" id="password" onChange={this.handleInputChange} value={this.state.password || ''} placeholder="Password" className="form-control" />
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Col sm={12} className="text-center"> 
-                                {this.state.txtMsj !== "" ? <Alert className={'alert '+this.state.AlertClass}>{this.state.txtMsj}</Alert> : "" }
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Col sm={12} className="text-center">
-                                <Button onClick={this.setForm} disabled={!this.state.disabledButton} className="btn-orange">Ingresar</Button>
-                            </Col>
-                        </FormGroup>
-                    </Form>
                 </Container>
             </>
         )
