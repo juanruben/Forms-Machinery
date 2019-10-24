f75fa37da90f3bafc689d44704a7dc248ef80260
Pablo Ruz <pruz@gux.cl>
2019-10-24 10:34:56 -0300

    Nueva ruta (registro historico)

--- src/Pages/Administrador/index.js
+++ src/Pages/Administrador/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './Components/Home';
+import RegistroHistorico from './Components/RegistroHistorico';
 
 class App extends Component {
 	render() {
@@ -9,6 +10,7 @@ class App extends Component {
             <Router>
                 <Switch> 
                     <Route exact path="/administrador/home/" component={Home}/>
+                    <Route exact path="/administrador/registro-historico/" component={RegistroHistorico}/>
                 </Switch>
             </Router>
         )
