cebc3c9c95ef5ea63638542381b6e152784d2637
Pablo Ruz <pruz@gux.cl>
2019-10-24 13:12:09 -0300

    Se agrega ruta agregar cliente

--- src/Pages/Administrador/index.js
+++ src/Pages/Administrador/index.js
@@ -4,15 +4,17 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home';
 import RegistroHistorico from './Components/RegistroHistorico';
 import ClienteLists from './Components/ClienteLists';
+import ClienteAgregar from './Components/ClienteAgregar';
 
 class App extends Component {
 	render() {
 		return (
             <Router>
-                <Switch> 
+                <Switch>
                     <Route exact path="/administrador/home/" component={Home}/>
                     <Route exact path="/administrador/registro-historico/" component={RegistroHistorico}/>
                     <Route exact path="/administrador/cliente/lists/" component={ClienteLists}/>
+                    <Route exact path="/administrador/cliente/agregar/" component={ClienteAgregar}/>
                 </Switch>
             </Router>
         )
