aa252f3aa514c2bca0db63cdd3188bd6260c48b6
juanruben <juanruben@gmail.com>
2019-10-24 13:35:07 -0300

    Corrige estilo

--- src/Service/Session.js
+++ src/Service/Session.js
@@ -1,33 +1,32 @@
 class Session {
-	openSession = (token, rut, nombre, telefono, email, perfil, NPerfil) => {
-		localStorage.setItem('token', token)
-		localStorage.setItem('rut', rut)
-		localStorage.setItem('nombre', nombre)
-		localStorage.setItem('telefono', telefono)
-		localStorage.setItem('email', email)
-		localStorage.setItem('perfil', perfil)
-		localStorage.setItem('NPerfil', NPerfil)
-		
-	}
-	isAuth= () => {
-		let isAuth = ("token" in localStorage)
-		return isAuth
-		 
-	}
-	getInfoUser = () => {
-		return {
-			token:localStorage.getItem("token"),
-			rut:localStorage.getItem("rut"),
-			nombre:localStorage.getItem("nombre"),
-			telefono:localStorage.getItem("telefono"),
-			email:localStorage.getItem("email"),
-			perfil:localStorage.getItem("perfil"),
-			NPerfil:1,
-			
-		}
-	}
-	close = () => {
-		localStorage.clear()		
-	}
+  openSession = (token, rut, nombre, telefono, email, perfil, NPerfil) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('rut', rut);
+    localStorage.setItem('nombre', nombre);
+    localStorage.setItem('telefono', telefono);
+    localStorage.setItem('email', email);
+    localStorage.setItem('perfil', perfil);
+    localStorage.setItem('NPerfil', NPerfil);
+  }
+
+  isAuth = () => {
+    const isAuth = ('token' in localStorage);
+    return isAuth;
+  }
+
+  getInfoUser = () => ({
+    token: localStorage.getItem('token'),
+    rut: localStorage.getItem('rut'),
+    nombre: localStorage.getItem('nombre'),
+    telefono: localStorage.getItem('telefono'),
+    email: localStorage.getItem('email'),
+    perfil: localStorage.getItem('perfil'),
+    NPerfil: 1,
+  })
+
+  close = () => {
+    localStorage.clear();
+  }
 }
-export default new Session()
+
+export default new Session();
