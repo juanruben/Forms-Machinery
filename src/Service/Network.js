b04b14be3777fb3f30db136bbc05012031db1531
juanruben <juanruben@gmail.com>
2019-10-24 14:01:06 -0300

    Corrige warnings

--- src/Service/Network.js
+++ src/Service/Network.js
@@ -6,88 +6,86 @@ import Session from './Session';
  */
 
 class Networking {
-    constructor () {
-		this.endpoint = ''
-		this.headers = {
-			mode: 'cors',
-			headers: {
-				'Content-Type': 'application/json',
-				'Accept': 'application/json',
-				'Authorization': Session.getInfoUser().token
-			}
-		}
-	}
-	
-    async get(endpoint,data={}) {
-		let qs = this.encodeQueryString(data);
-        this.endpoint = endpoint+ ((qs !== '') ? ('?' + qs) : '')
-		this.headers.method = 'GET'
-		return this.send()
-	}
+  constructor() {
+    this.endpoint = '';
+    this.headers = {
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: Session.getInfoUser().token,
+      },
+    };
+  }
 
-    async post(endpoint,data={}) {
-		this.endpoint = endpoint
-		this.headers.method = 'POST'
-		this.headers.body = this.fromData(data)
-		return this.send()
-	}
+  async get(endpoint, data = {}) {
+    const qs = this.encodeQueryString(data);
+    this.endpoint = endpoint + ((qs !== '') ? (`?${qs}`) : '');
+    this.headers.method = 'GET';
+    return this.send();
+  }
 
-    async send() {
-		let endpoint = baseUrlApi + this.endpoint
-		let response = await fetch(endpoint, this.headers)
-        let result = await response.json()
-		if (result) {
-			return result;
-		}
-		return null
-	}
-	
-    fromData(data){
-		let body = '';
-		if (typeof data['form-data'] === 'boolean'){
-			body = new FormData()
-			for(var name in data) {
-				if(name!=='form-data'){
-				    body.append(name, data[name])
-				}
-			}
-		}else{
-			body = JSON.stringify(data)
-        }
-		return body;
+  async post(endpoint, data = {}) {
+    this.endpoint = endpoint;
+    this.headers.method = 'POST';
+    this.headers.body = this.fromData(data);
+    return this.send();
+  }
+
+  async send() {
+    const endpoint = baseUrlApi + this.endpoint;
+    const response = await fetch(endpoint, this.headers);
+    const result = await response.json();
+    if (result) {
+      return result;
     }
+    return null;
+  }
 
-	encodeQueryStringFromArray(name, params, index) {
-		const keys = Object.keys(params)
-		return keys.length
-		? keys
-		.map(key => name + '[' + index + '][' + encodeURIComponent(key) + ']='
-		+ encodeURIComponent(params[key]))
-		.join("&")
-		: ""
-	}
-	
-	encodeQueryString(filters){
-		var url = '';
-		Object.keys(filters).forEach(function(key) {
-			if (Array.isArray(filters[key])) {
-				filters[key].map(function (item, index) {
-					if(item!==null){
-						url +=key+'='+item+'&'
-					}
-					return true
-				},this);
-			}
-			else {
-				if (filters[key]) {
-					url += `${key}=${filters[key]}&`
-				}
-			}
-		},this)
-		return url.substring(0, url.length-1);
-	}
+  static fromData(data) {
+    let body = '';
+    if (typeof data['form-data'] === 'boolean') {
+      body = new FormData();
+
+      data.forEach((name) => {
+        if (name !== 'form-data') {
+          body.append(name, data[name]);
+        }
+      });
+    } else {
+      body = JSON.stringify(data);
+    }
+    return body;
+  }
+
+  static encodeQueryStringFromArray(name, params, index) {
+    const keys = Object.keys(params);
+    return keys.length
+      ? keys
+        .map((key) => `${name}[${index}][${encodeURIComponent(key)}]=${
+          encodeURIComponent(params[key])}`)
+        .join('&')
+      : '';
+  }
+
+  static encodeQueryString(filters) {
+    let url = '';
+    Object.keys(filters).forEach((key) => {
+      if (Array.isArray(filters[key])) {
+        filters[key].map((item) => {
+          if (item !== null) {
+            url += `${key}=${item}&`;
+          }
+          return true;
+        }, this);
+      } else if (filters[key]) {
+        url += `${key}=${filters[key]}&`;
+      }
+    }, this);
+    return url.substring(0, url.length - 1);
+  }
 }
 
-var Network = new Networking();
+const Network = new Networking();
 
-export default Network
+export default Network;
