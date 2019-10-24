f7ae14818d1a5ab1c3971fda2cbfb16856f6175a
juanruben <juanruben@gmail.com>
2019-10-24 12:37:32 -0300

    Refactoriza componente stateless

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from './Pages/Login';
+import Recuperar from './Pages/Recuperar';
+import Administrador from './Pages/Administrador';
+import Operador from './Pages/Operador';
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/Recuperar" component={Recuperar} />
+      <Route path="/administrador/" component={Administrador} />
+      <Route path="/operador/" component={Operador} />
+    </Switch>
+  </Router>
+);
+
+export default App;
