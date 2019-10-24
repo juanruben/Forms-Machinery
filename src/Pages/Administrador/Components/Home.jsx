7845b932fa29dc86b1ba5c4311e21daf5a6a1b33
juanruben <juanruben@gmail.com>
2019-10-24 12:39:47 -0300

    Refactoriza componente stateless

---
+++ src/Pages/Administrador/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+import Layout from '../../../Layout';
+
+const Home = () => (
+  <Layout name="Esto es el topheader">
+    <Container>
+      <Row>
+        <Col>
+          Esto es el Home del administrador
+        </Col>
+      </Row>
+    </Container>
+  </Layout>
+);
+
+export default Home;
