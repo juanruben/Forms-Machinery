31a60f4c3030a00227aa86d4074a04be2a46b048
juanruben <juanruben@gmail.com>
2019-10-24 12:57:06 -0300

    Refactoriza componente stateless

--- src/Layout/Components/NavBarTop.jsx
+++ src/Layout/Components/NavBarTop.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Row, Col} from 'reactstrap';
 
-export default class NavBarTop extends React.Component {
+class NavBarTop extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
 
     };
   }
+
   render() {
+    const { nameNavBarTop } = this.props;
     return (
       <div className="NavBarTop">
         <Row>
           <Col xs={12} md={12} className="text-left">
-            <p className="navBarTextTitle">{this.props.nameNavBarTop}</p>
+            <p className="navBarTextTitle">{nameNavBarTop}</p>
           </Col>
         </Row>
       </div>
     );
   }
 }
+
+export default NavBarTop;
