43d9b06226bce85e2f9078c8d1552041ffbd7c91
Pablo Ruz <pruz@gux.cl>
2019-10-28 15:49:31 -0300

    Se agrega ruta

--- src/Pages/Administrador/App.jsx
+++ src/Pages/Administrador/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home';
-import RegistroHistorico from './Components/RegistroHistorico';
 import ClientLists from './Components/Client/ClientLists';
 import ClientAdd from './Components/Client/ClientAdd';
 import UserLists from './Components/User/UserLists';
@@ -10,12 +9,13 @@ import FormAdd from './Components/Form/FormAdd';
 import FormLists from './Components/Form/FormLists';
 import MachineryLists from './Components/Machinery/MachineryLists';
 import MachineryAdd from './Components/Machinery/MachineryAdd';
+import HistoricalRecord from './Components/HistoricalRecord/HistoricalRecord';
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/administrador/home/" component={Home} />
-      <Route exact path="/administrador/registro-historico/" component={RegistroHistorico} />
+      <Route exact path="/administrador/registro-historico/" component={HistoricalRecord} />
       <Route exact path="/administrador/cliente/add/" component={ClientAdd} />
       <Route exact path="/administrador/cliente/lists/" component={ClientLists} />
       <Route exact path="/administrador/usuario/add/" component={UserAdd} />
