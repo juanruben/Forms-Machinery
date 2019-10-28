d1c042111d27d269c08d33d54d80c6c7c04e705d
Pablo Ruz <pruz@gux.cl>
2019-10-28 09:43:52 -0300

    Se agregan nuevas rutas

--- src/Pages/Administrador/App.jsx
+++ src/Pages/Administrador/App.jsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home';
 import RegistroHistorico from './Components/RegistroHistorico';
-import ClienteAgregar from './Components/ClienteAgregar';
-import ClienteLists from './Components/ClienteLists';
+import ClientLists from './Components/Client/ClientLists';
+import ClientAdd from './Components/Client/ClientAdd';
+import UserLists from './Components/User/UserLists';
+import UserAdd from './Components/User/UserAdd';
+import MachineAdd from './Components/Machine/MachineAdd';
+import MachineLists from './Components/Machine/MachineLists';
+import FormAdd from './Components/Form/FormAdd';
+import FormLists from './Components/Form/FormLists';
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/administrador/home/" component={Home} />
       <Route exact path="/administrador/registro-historico/" component={RegistroHistorico} />
-      <Route exact path="/administrador/cliente/agregar/" component={ClienteAgregar} />
-      <Route exact path="/administrador/cliente/lists/" component={ClienteLists} />
+      <Route exact path="/administrador/cliente/add/" component={ClientAdd} />
+      <Route exact path="/administrador/cliente/lists/" component={ClientLists} />
+      <Route exact path="/administrador/usuario/add/" component={UserAdd} />
+      <Route exact path="/administrador/usuario/lists/" component={UserLists} />
+      <Route exact path="/administrador/maquina/add/" component={MachineAdd} />
+      <Route exact path="/administrador/maquina/lists/" component={MachineLists} />
+      <Route exact path="/administrador/formulario/add/" component={FormAdd} />
+      <Route exact path="/administrador/formulario/lists/" component={FormLists} />
     </Switch>
   </Router>
 );
