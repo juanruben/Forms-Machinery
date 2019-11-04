a53530b8f9277b9ee580812ac3520e6c98190e63
juanruben <juanruben@gmail.com>
2019-11-04 13:33:49 -0300

    Refactoriza test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,15 +6,14 @@ import App from './App';
 import AppRouter from './AppRouter';
 
 describe('App', () => {
-    let wrapper = shallow(<App />);
+    let wrapper;
+    beforeEach(() => { wrapper = shallow(<App />); });
 
     it('renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<App />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
-
-    wrapper = shallow(<App />);
 
     it('should render the AppRouter Component', () => {
         expect(wrapper.containsMatchingElement(<AppRouter />)).toEqual(true);
