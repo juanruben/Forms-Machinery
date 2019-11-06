50a599ee266c08edeaaec1053b0be0ea826f9929
juanruben <juanruben@gmail.com>
2019-11-06 18:49:26 -0300

    Refactoriza estilos

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './Assets/css/main.scss';
+import './styles.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
