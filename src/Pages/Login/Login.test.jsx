4c36810a965c4ef6119e48e7a72d2aad46a8d509
juanruben <juanruben@gmail.com>
2019-11-09 13:07:44 -0300

    Agrega tests del componente Login

--- src/Pages/Login/Login.test.jsx
+++ src/Pages/Login/Login.test.jsx
@@ -1,62 +1,104 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import { StateProvider } from '../../State';
+import { initialState, reducer } from '../../App';
+import AppWithLoader from '../../AppWithLoader';
 import Login from './Login';
-import Layout from '../../Layout/LayoutFullWidth/LayoutFullWidth';
-import Logo from '../../Components/Logo/Logo';
-import Input from '../../Components/Input/Input';
-import Button from '../../Components/Button/Button';
 
-// jest.mock('../Service/LoginApi');
+jest.mock('./../../Service/Api');
 
 describe('Login', () => {
     let wrapper;
+    let wrapperLogin;
     beforeEach(() => {
-        wrapper = shallow(<Login />);
+        wrapper = mount(
+            <StateProvider initialState={initialState} reducer={reducer}>
+                <AppWithLoader />
+            </StateProvider>,
+        );
+        wrapperLogin = wrapper.find(Login).at(0);
     });
 
-    // it('should render correctly', => expect(wrapper.debug()).toMatchSnapshot());
+    it('should render correctly', () => expect(wrapper.debug()).toMatchSnapshot());
 
-    // it('test api', (done) => {
-    //     console.log("holiholi");
-    //     setTimeout(() => {
-    //         wrapper.setState({ username: 'testtest', password: 'testtest' });
-    //         wrapper.instance().login();
-    //         wrapper.update();
-    //         expect(wrapper.find('div')).toHaveLength(1);
-    //         done();
-    //     });
-    // });
+    it('should show and hide loading', () => {
+        act(() => {
+            wrapperLogin.instance().toggleLoading(true);
+        });
+        wrapper.update();
+        expect(wrapper.find('Loader').prop('visible')).toBeTruthy();
+        act(() => {
+            wrapperLogin.instance().toggleLoading(false);
+        });
+        wrapper.update();
+        expect(wrapper.find('Loader').prop('visible')).toBeFalsy();
+    });
 
-    // it('username check', () => {
-    //     console.log("AQUI", wrapper.debug());
-    //     const aaa = wrapper.find('input');//.simulate('change', { target: { name: 'username', value: 'krishankantsinghal' } });
-    //     console.log("AQUI", aaa.debug());
-    //     // expect(wrapper.state('username')).toEqual('krishankantsinghal');
-    // });
+    it('should call functions on login with no data', () => {
+        const spyValidation = jest.spyOn(wrapperLogin.instance(), 'handleValidation');
+        const spyLoader = jest.spyOn(wrapperLogin.instance(), 'toggleLoading');
+        act(() => {
+            wrapperLogin.instance().handleSignIn();
+        });
+        expect(spyValidation).toBeCalledTimes(1);
+        expect(spyLoader).toBeCalledTimes(0);
+    });
 
     it('should update state', () => {
-        wrapper.instance().handleInputChange({ target: { name: 'username', value: 'test' } });
-        expect(wrapper.state('username')).toEqual('test');
-        wrapper.instance().handleInputChange({ target: { name: 'password', value: 'test2' } });
-        expect(wrapper.state('password')).toEqual('test2');
+        wrapperLogin.instance().handleInputChange({ target: { name: 'username', value: 'test' } });
+        expect(wrapperLogin.state('username')).toEqual('test');
+        wrapperLogin.instance().handleInputChange({ target: { name: 'password', value: 'test2' } });
+        expect(wrapperLogin.state('password')).toEqual('test2');
     });
 
     it('should validate', () => {
-        wrapper.setState({ username: 'test', password: 'test' });
-        expect(wrapper.instance().handleValidation()).toBe(true);
-        wrapper.setState({ username: '' });
-        expect(wrapper.instance().handleValidation()).toBe(false);
-        wrapper.setState({ password: '' });
-        expect(wrapper.instance().handleValidation()).toBe(false);
+        wrapperLogin.setState({ username: 'test', password: 'Test123' });
+        expect(wrapperLogin.instance().handleValidation()).toBe(true);
+        wrapperLogin.setState({ username: '' });
+        expect(wrapperLogin.instance().handleValidation()).toBe(false);
+        wrapperLogin.setState({ password: '' });
+        expect(wrapperLogin.instance().handleValidation()).toBe(false);
     });
 
-    it('should have a full width container', () => {
-        expect(wrapper.find(Layout)).toHaveLength(1);
+    it('should have a box container', () => {
+        expect(wrapper.find('.box-container')).toHaveLength(1);
     });
 
-    it('should have a logo and two inputs', () => {
-        expect(wrapper.find(Logo)).toHaveLength(1);
-        expect(wrapper.find(Input)).toHaveLength(2);
-        expect(wrapper.find(Button)).toHaveLength(1);
+    it('should have a logo, two inputs and a button', () => {
+        expect(wrapper.find('Logo')).toHaveLength(1);
+        expect(wrapper.find('Input')).toHaveLength(3);
+        expect(wrapper.find('Button')).toHaveLength(1);
     });
+
+    afterEach(() => wrapper.unmount());
+});
+
+describe('Login process', () => {
+    let wrapper;
+    let wrapperLogin;
+    beforeEach(() => {
+        wrapper = mount(
+            <StateProvider initialState={initialState} reducer={reducer}>
+                <AppWithLoader />
+            </StateProvider>,
+        );
+        wrapperLogin = wrapper.find(Login).at(0);
+    });
+
+    it('should log in', () => {
+        wrapperLogin.setState({ username: 'test', password: 'Test123' });
+        const spyValidation = jest.spyOn(wrapperLogin.instance(), 'handleValidation');
+        const spyLoader = jest.spyOn(wrapperLogin.instance(), 'toggleLoading');
+
+        act(() => {
+            wrapperLogin.instance().handleSignIn();
+        });
+        expect(spyValidation).toBeCalledTimes(1);
+        expect(spyLoader).toBeCalledTimes(2);
+        wrapper.update();
+        expect(wrapper.render().find('Main')).toBeTruthy();
+    });
+
+    afterEach(() => wrapper.unmount());
 });
