3845052e33c5fcf10788ca3ebff6fda31aefe8b7
juanruben <juanruben@gmail.com>
2019-11-09 20:25:36 -0300

    Corrige método http y ruta de endpoint

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function login(username, password) {
-    const response = await axios.get(`${process.env.REACT_APP_API_URL}/todos/1`, {
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, {
         params: {
             username,
             password,
