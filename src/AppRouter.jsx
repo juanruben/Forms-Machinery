7adb3ec3ba4c60900d9be772c4f1c5c24155e948
juanruben <juanruben@gmail.com>
2019-11-09 13:11:50 -0300

    Gestiona rutas privadas

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-// import Login from './Pages/Login';
-// import Recuperar from './Pages/Recover';
-// import Administrador from './Pages/Administrador/App';
-// import Operador from './Pages/Operador/App';
+import {
+    BrowserRouter as Router, Route, Switch, Redirect,
+} from 'react-router-dom';
+import { useStateValue } from './State';
+import Login from './Pages/Login/Login';
+import Recover from './Pages/Recover/Recover';
+import Main from './Pages/Main';
+import Page404 from './Components/Page404';
+
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+    const [{ loggedin }] = useStateValue();
+    if (loggedin) {
+        return (
+            <Route component={() => <Component {...rest} />} />
+        );
+    }
+    return <Redirect to="/login" />;
+};
 
 const AppRouter = () => (
     <Router>
         <Switch>
-            {/* <Route exact path="/" component={Login} />
-            <Route exact path="/Recuperar" component={Recuperar} />
-            <Route path="/administrador/" component={Administrador} />
-            <Route path="/operador/" component={Operador} /> */}
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/recuperar" component={Recover} />
+            <PrivateRoute path="/admin" component={Main} />
+            <PrivateRoute path="/op" component={Main} />
+            <PrivateRoute path="/" component={Main} />
+            <Route component={Page404} />
         </Switch>
     </Router>
 );
