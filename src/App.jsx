f6b52609237fdb772896f0c65daba6da7ed3e558
juanruben <juanruben@gmail.com>
2019-11-09 13:10:20 -0300

    Agrega Provider a App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,76 @@
 import React from 'react';
-import AppRouter from './AppRouter';
+import { StateProvider } from './State';
+import { encode, decode } from './Service/Utils';
+import AppWithLoader from './AppWithLoader';
 
+import './styles.scss';
+
+export const initialState = {
+    token: null,
+    loggedin: false,
+    loading: false,
+    role: null,
+};
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_LOADING':
+            return {
+                ...state,
+                loading: action.value,
+            };
+
+        case 'SIGN_IN':
+            localStorage.setItem('t', encode(action.value.token));
+            localStorage.setItem('r', encode(action.value.role));
+            return {
+                ...state,
+                loggedin: true,
+                token: action.value.token,
+                role: action.value.role,
+            };
+
+        case 'LOAD_SESSION': {
+            let tokenSession = localStorage.getItem('t');
+            let roleSession = localStorage.getItem('r');
+
+            try {
+                tokenSession = decode(tokenSession);
+                roleSession = decode(roleSession);
+            } catch (e) {
+                localStorage.clear();
+                return {
+                    ...state,
+                    loggedin: false,
+                    token: null,
+                };
+            }
+
+            return {
+                ...state,
+                loggedin: true,
+                token: tokenSession,
+                role: parseInt(roleSession),
+            };
+        }
+
+        case 'EXIT':
+            localStorage.clear();
+            return {
+                ...state,
+                loggedin: false,
+                token: null,
+            };
+
+        default:
+            return state;
+    }
+};
 
 const App = () => (
-    <AppRouter />
+    <StateProvider initialState={initialState} reducer={reducer}>
+        <AppWithLoader />
+    </StateProvider>
 );
 
 export default App;
