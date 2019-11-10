5422db947044c8aff1566332290d057f4b2872f0
juanruben <juanruben@gmail.com>
2019-11-10 20:18:29 -0300

    Refactoriza componente

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Admin extends Component {
-	render() {
-		return (
-			<div>
-				soy admin
-			</div>
-		);
-	}
+function Admin() {
+    return (
+        <div>
+            soy admin
+        </div>
+    );
 }
 
 export default Admin;
