f6078c8b49ac764e8299dd04cd696f36b5223624
juanruben <juanruben@gmail.com>
2019-11-10 09:13:57 -0300

    Corrige bug con el estado

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,45 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { StateContext } from '../State';
+import { useStateValue } from '../State';
 import Admin from './Admin';
 import Operator from './Operator';
 import Page404 from '../Components/Page404';
 
-class Main extends Component {
-    // const [{ role }, dispatch] = useStateValue();
-
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        const [, dispatch] = this.context;
+function Main() {
+    const context = useStateValue();
+    function handleClick() {
+        const [, dispatch] = context;
         dispatch({
             type: 'EXIT',
         });
     }
 
     // const View = role === 1 ? <Admin /> : <Operator />;
-    render() {
-        const [{ role }] = this.context;
-        return (
-            <>
-                fjadslfa:
-                {role}
-                <button type="submit" onClick={this.handleClick}>
-                    Salir
-                </button>
-                <Switch>
-                    <Route path="/admin" component={Admin} />
-                    <Route path="/op" component={Operator} />
-                    <Route component={Page404} />
-                </Switch>
-            </>
-        );
-    }
+    return (
+        <>
+            fjadslf:
+            <button type="submit" onClick={handleClick}>
+                Salir
+            </button>
+            <Switch>
+                <Route path="/admin" component={Admin} />
+                <Route path="/op" component={Operator} />
+                <Route component={Page404} />
+            </Switch>
+        </>
+    );
 }
-
-Main.contextType = StateContext;
 
 export default Main;
