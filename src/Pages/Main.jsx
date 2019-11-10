79afe1c4af8c973119d896fa055a0f40b5c93804
juanruben <juanruben@gmail.com>
2019-11-10 20:32:03 -0300

    Agrega sidebar y area de contenido principal

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,33 +1,80 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Sidebar from 'react-sidebar';
 import { useStateValue } from '../State';
+import Menu from '../Components/Menu';
 import Admin from './Admin';
 import Operator from './Operator';
-import Page404 from '../Components/Page404';
 
-function Main() {
-    const context = useStateValue();
-    function handleClick() {
-        const [, dispatch] = context;
-        dispatch({
-            type: 'EXIT',
+import dataMenu from './dataMenuAdmin';
+
+const mql = window.matchMedia('(min-width: 800px)');
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            docked: mql.matches,
+            trans: false,
+        };
+
+        this.menuButtonClick = this.menuButtonClick.bind(this);
+        this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+    }
+
+    componentDidMount() {
+        mql.addListener(this.mediaQueryChanged);
+    }
+
+    componentWillUnmount() {
+        mql.removeListener(this.mediaQueryChanged);
+    }
+
+    mediaQueryChanged() {
+        this.setState({
+            docked: mql.matches,
         });
     }
 
-    // const View = role === 1 ? <Admin /> : <Operator />;
-    return (
-        <>
-            fjadslf:
-            <button type="submit" onClick={handleClick}>
-                Salir
-            </button>
-            <Switch>
-                <Route path="/admin" component={Admin} />
-                <Route path="/op" component={Operator} />
-                <Route component={Page404} />
-            </Switch>
-        </>
-    );
+    menuButtonClick(event) {
+        event.preventDefault();
+        const { docked } = this.state;
+        this.setState({
+            trans: true,
+            docked: !docked,
+        });
+    }
+
+    render() {
+        const { docked, trans } = this.state;
+
+        const sidebarProps = {
+            sidebar: <Menu items={dataMenu} />,
+            docked,
+            touch: true,
+            transitions: trans,
+            touchHandleWidth: 20,
+            dragToggleDistance: 30,
+        };
+
+        return (
+            <Sidebar {...sidebarProps}>
+                <button onClick={this.menuButtonClick}>Menú</button>
+                Contenido....
+                <Switch>
+                    <Route exact path="/admin/clientes" component={Admin} />
+                    <Route exact path="/admin/usuarios" component={Admin} />
+                    <Route exact path="/admin/maquinas" component={Admin} />
+                    <Route exact path="/admin/formularios" component={Admin} />
+                    <Route exact path="/admin" component={Admin} />
+                    <Route exact path="/entrada" component={Operator} />
+                    <Route exact path="/salida" component={Operator} />
+                    <Route exact path="/" component={Operator} />
+                </Switch>
+            </Sidebar>
+        );
+    }
 }
 
 export default Main;
