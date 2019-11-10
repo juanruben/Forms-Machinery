94c17ad962cd14b3cd65f5a453788d19841c6eca
juanruben <juanruben@gmail.com>
2019-11-09 21:07:59 -0300

    Captura excepción

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 export async function login(username, password) {
-    const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, {
-        params: {
-            username,
-            password,
-        },
-    });
-    return response;
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, {
+            params: {
+                username,
+                password,
+            },
+        });
+        return response;
+    } catch (e) {
+        return null;
+    }
 }
