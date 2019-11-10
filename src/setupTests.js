20abcf02fea4f8a0ae5943d6f901672e3f69b4d4
juanruben <juanruben@gmail.com>
2019-11-10 20:58:08 -0300

    Agrega mock de matchMedia

--- src/setupTests.js
+++ src/setupTests.js
@@ -10,3 +10,16 @@ const localStorageMock = {
 global.localStorage = localStorageMock;
 
 configure({ adapter: new Adapter() });
+
+window.matchMedia = jest.fn().mockImplementation((query) => {
+    return {
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    };
+});
