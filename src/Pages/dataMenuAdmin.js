65933e09f92414c0d3d596a7e518329137fe49bf
juanruben <juanruben@gmail.com>
2019-11-10 20:30:53 -0300

    Crea archivos de items de menu

---
+++ src/Pages/dataMenuAdmin.js
@@ -0,0 +1,42 @@
+export default [
+    {
+        id: 1,
+        title: 'Dashboard',
+        path: '/admin',
+    },
+    {
+        id: 2,
+        title: 'Clientes',
+        path: '/admin/clientes',
+    },
+    {
+        id: 3,
+        title: 'Usuarios',
+        path: '/admin/usuarios',
+    },
+    {
+        id: 4,
+        title: 'Máquinas',
+        path: '/admin/maquinas',
+    },
+    {
+        id: 5,
+        title: 'Formularios',
+        path: '/admin/formularios',
+    },
+    {
+        id: 6,
+        title: 'Dashboard Op',
+        path: '/',
+    },
+    {
+        id: 7,
+        title: 'Registrar Entrada',
+        path: '/entrada',
+    },
+    {
+        id: 8,
+        title: 'Registrar Salida',
+        path: '/salida',
+    },
+];
---
+++ src/Pages/dataMenuOp.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 1,
+        title: 'Dashboard',
+        path: '/',
+    },
+    {
+        id: 2,
+        title: 'Registrar Entrada',
+        path: '/entrada',
+    },
+    {
+        id: 3,
+        title: 'Registrar Salida',
+        path: '/salida',
+    },
+];
