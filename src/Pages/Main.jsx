57c35c3f278bad0205970b6a6c432ce1bd33b105
juanruben <juanruben@gmail.com>
2019-11-10 23:23:38 -0300

    Agrega type a button

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Sidebar from 'react-sidebar';
-import { useStateValue } from '../State';
+// import { useStateValue } from '../State';
 import Menu from '../Components/Menu/Menu';
 import Admin from './Admin';
 import Operator from './Operator';
@@ -59,18 +59,18 @@ class Main extends React.Component {
         return (
             <Sidebar {...sidebarProps}>
                 <div className="content-area">
-                <button onClick={this.menuButtonClick}>Menú</button>
-                Contenido....
-                <Switch>
-                    <Route exact path="/admin/clientes" component={Admin} />
-                    <Route exact path="/admin/usuarios" component={Admin} />
-                    <Route exact path="/admin/maquinas" component={Admin} />
-                    <Route exact path="/admin/formularios" component={Admin} />
-                    <Route exact path="/admin" component={Admin} />
-                    <Route exact path="/entrada" component={Operator} />
-                    <Route exact path="/salida" component={Operator} />
-                    <Route exact path="/" component={Operator} />
-                </Switch>
+                    <button onClick={this.menuButtonClick} type="button">Menú</button>
+                    Contenido....
+                    <Switch>
+                        <Route exact path="/admin/clientes" component={Admin} />
+                        <Route exact path="/admin/usuarios" component={Admin} />
+                        <Route exact path="/admin/maquinas" component={Admin} />
+                        <Route exact path="/admin/formularios" component={Admin} />
+                        <Route exact path="/admin" component={Admin} />
+                        <Route exact path="/entrada" component={Operator} />
+                        <Route exact path="/salida" component={Operator} />
+                        <Route exact path="/" component={Operator} />
+                    </Switch>
                 </div>
             </Sidebar>
         );
