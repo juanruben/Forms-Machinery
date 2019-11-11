ff7997d5aa9ab2f520de748c24703b78419e302b
juanruben <juanruben@gmail.com>
2019-11-10 21:13:43 -0300

    Agrega tests para cantidad de elementos

--- src/Components/Menu.test.jsx
+++ src/Components/Menu.test.jsx
@@ -2,7 +2,43 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Menu from './Menu';
 
-test('should render ok', () => {
-    const wrapper = shallow(<Menu items={[]} />);
-    expect(wrapper.debug()).toMatchSnapshot();
+describe('Menu', () => {
+    it('should render ok', () => {
+        const wrapper = shallow(<Menu items={[]} />);
+        expect(wrapper.debug()).toMatchSnapshot();
+    });
+
+    test('several items', () => {
+        const items = [
+            {
+                id: 1,
+                title: 'Dashboard',
+                path: '/admin',
+            },
+            {
+                id: 2,
+                title: 'Clientes',
+                path: '/admin/clientes',
+            },
+        ];
+        const wrapper = shallow(<Menu items={items} />);
+        expect(wrapper.find('Link')).toHaveLength(2);
+    });
+
+    test('one item', () => {
+        const items = [
+            {
+                id: 1,
+                title: 'Dashboard',
+                path: '/admin',
+            },
+        ];
+        const wrapper = shallow(<Menu items={items} />);
+        expect(wrapper.find('Link')).toHaveLength(1);
+    });
+
+    test('zero items', () => {
+        const wrapper = shallow(<Menu items={[]} />);
+        expect(wrapper.find('Link')).toHaveLength(0);
+    });
 });
