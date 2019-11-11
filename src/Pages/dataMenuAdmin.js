cda1dc8d075111ef6f374567fc33d407a449aee2
juanruben <juanruben@gmail.com>
2019-11-11 14:47:20 -0300

    Agrega ícono a los items

--- src/Pages/dataMenuAdmin.js
+++ src/Pages/dataMenuAdmin.js
@@ -3,40 +3,56 @@ export default [
         id: 1,
         title: 'Dashboard',
         path: '/admin',
+        icon: 'fas fa-user-tie',
+
     },
     {
         id: 2,
         title: 'Clientes',
         path: '/admin/clientes',
+        icon: 'fas fa-user-tie',
+
     },
     {
         id: 3,
         title: 'Usuarios',
         path: '/admin/usuarios',
+        icon: 'fas fa-user-tie',
+
     },
     {
         id: 4,
         title: 'Máquinas',
         path: '/admin/maquinas',
+        icon: 'fas fa-user-tie',
+
     },
     {
         id: 5,
         title: 'Formularios',
         path: '/admin/formularios',
+        icon: 'fas fa-user-tie',
+
     },
     {
         id: 6,
         title: 'Dashboard Op',
         path: '/',
+        icon: 'fas fa-user-tie',
+
     },
     {
         id: 7,
         title: 'Registrar Entrada',
         path: '/entrada',
+        icon: 'fas fa-user-tie',
+
     },
     {
         id: 8,
         title: 'Registrar Salida',
         path: '/salida',
+        icon: 'fas fa-user-tie',
+
     },
 ];
