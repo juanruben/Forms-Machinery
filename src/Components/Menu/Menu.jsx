2b8ae0f5ee229d98e3457ade1828b3a55f375bcb
juanruben <juanruben@gmail.com>
2019-11-10 22:21:34 -0300

    Agrega botón de salir y su estilo

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { useStateValue } from '../../State';
 import './Menu.scss';
 
 const Menu = (props) => {
     const { items } = props;
+    const [, dispatch] = useStateValue();
+
+    const test = () => {
+        dispatch({
+            type: 'EXIT',
+        });
+    };
+
     return (
         <div className="sidebar">
             <div className="content">
                 {items.map((item) => (
                     <Link key={item.id} to={item.path} className="sidebarLink">{item.title}</Link>
                 ))}
+                <button type="button" onClick={test}>Salir</button>
             </div>
         </div>
     );
