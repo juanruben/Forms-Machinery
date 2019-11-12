7908d2c38673109ab87f45050e3531b24fa1ded2
juanruben <juanruben@gmail.com>
2019-11-12 14:24:24 -0300

    Crea componente para gestionar maquinaria

---
+++ src/Pages/Machines/Machines.jsx
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+// import { StateContext } from '../State';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import IconButton from '../../Components/IconButton/IconButton';
+import MachineForm from './MachineForm';
+import ModalAdd from '../../Components/ModalAdd/ModalAdd';
+
+import './Machines.scss';
+
+class Machines extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        const data = [
+            {
+                name: 'AAA',
+                age: 21,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'BBB',
+                age: 26,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'CCCC',
+                age: 36,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'AAA',
+                age: 21,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'BBB',
+                age: 26,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'CCCC',
+                age: 36,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'AAA',
+                age: 21,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'BBB',
+                age: 26,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'CCCC',
+                age: 36,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'AAA',
+                age: 21,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'BBB',
+                age: 26,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+            {
+                name: 'CCCC',
+                age: 36,
+                friend: {
+                    name: 'Jason Maurer',
+                    age: 23,
+                },
+            },
+        ];
+
+        const columns = [{
+            Header: 'Name',
+            accessor: 'name', // String-based value accessors!
+        }, {
+            Header: 'Age',
+            accessor: 'age',
+            Cell: (props) => <span className="number">{props.value}</span>, // Custom cell components!
+        }, {
+            id: 'friendName', // Required because our accessor is not a string
+            Header: 'Friend Name',
+            accessor: (d) => d.friend.name, // Custom value accessors!
+        }, {
+            Header: (props) => <span>Friend Age</span>, // Custom header components!
+            accessor: 'friend.age',
+        }];
+
+        return (
+            <>
+                <div className="top-bar">
+                    <IconButton onClick={() => { }} icon="far fa-file-excel" />
+                    <ModalAdd form={<MachineForm />} title="Crear máquina" />
+                </div>
+
+                <ReactTable
+                    data={data}
+                    columns={columns}
+                    filterable
+                    className="-striped"
+                    defaultPageSize={10}
+                    previousText="Anterior"
+                    nextText="Siguiente"
+                    loadingText="Cargando"
+                    noDataText="Sin datos"
+                    pageText="Página"
+                    ofText="de"
+                    rowsText="usuarios"
+                />
+            </>
+        );
+    }
+}
+
+export default Machines;
