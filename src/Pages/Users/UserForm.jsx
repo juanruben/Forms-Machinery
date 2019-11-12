8c487f6d1e68001a8375efa41a2585c43ae5e7fc
juanruben <juanruben@gmail.com>
2019-11-12 14:25:46 -0300

    Crea componentes para gestionar usuarios

---
+++ src/Pages/Users/UserForm.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
+import Input from '../../Components/Input/Input';
+import Button from '../../Components/Button/Button';
+
+class UserForms extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <>
+                <Input placeholder="Nombre empresa" />
+                <Input placeholder="Razón social" />
+                <Input placeholder="Rut empresa" />
+                <Input placeholder="Teléfono de contacto" />
+                <Input placeholder="Dirección" />
+                <Input placeholder="Emails" />
+                * Estas direcciones de correo recibirán reporte y estado de las maquinarias cuando esta salgan del taller hacia la obra del cliente
+                <Row>
+                    <Col md={8} />
+                    <Col md={4}>
+                        <Button text="Crear" />
+                    </Col>
+                </Row>
+            </>
+        );
+    }
+}
+
+export default UserForms;
