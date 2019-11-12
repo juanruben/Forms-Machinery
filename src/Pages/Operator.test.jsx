3ab4171576a8d4275f287072e8a70649000f5746
juanruben <juanruben@gmail.com>
2019-11-12 14:25:31 -0300

    Elimina componente innecesario

--- src/Pages/Operator.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Operator from './Operator';
-
-test('should render ok', () => {
-    const wrapper = shallow(<Operator />);
-    expect(wrapper.debug()).toMatchSnapshot();
-});
