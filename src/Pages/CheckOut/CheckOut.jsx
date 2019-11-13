e4cb210d2bf01498cf1cc53be22b5ee67b715a3b
juanruben <juanruben@gmail.com>
2019-11-13 16:30:12 -0300

    Crea inputs temporales

--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Row, Col } from 'reactstrap';
+import Title from '../../Components/Title/Title';
+import Input from '../../Components/Input/Input';
+import Button from '../../Components/Button/Button';
+import './CheckOut.scss';
 
 function CheckOut() {
     return (
-        <div>
-            CheckOut
+        <div className="check-in-container">
+            <Title text="Datos generales" />
+            <div className="check-in-container__section">
+                <Row>
+                    <Col md={6}><Input label="Cliente" placeholder="Seleccione..." /></Col>
+                    <Col md={6}><Input label="Obra" placeholder="Seleccione..." /></Col>
+                    <Col md={6}><Input label="Máquina" placeholder="Seleccione..." /></Col>
+                    <Col md={6}><Input label="Código" placeholder="Seleccione..." /></Col>
+                </Row>
+            </div>
+            <Row>
+                <Col md={8} />
+                <Col md={4}>
+                    <Button text="Enviar" />
+                </Col>
+            </Row>
         </div>
     );
 }
