e6e72635525d908aa28df5165be01ec468aec9f3
juanruben <juanruben@gmail.com>
2019-11-13 09:58:40 -0300

    Crea componente para secciones de formulario

---
+++ src/Pages/Forms/Sections.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sections extends Component {
+    render() {
+        return (
+            <div>
+                Sección
+            </div>
+        );
+    }
+}
+
+export default Sections;
