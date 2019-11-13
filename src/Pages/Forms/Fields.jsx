38c46c2197de7ea28f67b53d280f47296fdda205
juanruben <juanruben@gmail.com>
2019-11-13 12:38:38 -0300

    Crea comp para gestionar campos de formulario

---
+++ src/Pages/Forms/Fields.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Fields extends Component {
+    render() {
+        return (
+            <div>
+                afsdfasd
+            </div>
+        );
+    }
+}
+
+export default Fields;
