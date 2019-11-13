2821a2b83c83467ebee031049e9a07bd3e56db12
juanruben <juanruben@gmail.com>
2019-11-13 08:13:57 -0300

    Crea archivo de configuraciones generales

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export const tableConfig = {
+    filterable: true,
+    className: '-striped',
+    defaultPageSize: 10,
+    previousText: '<',
+    nextText: '>',
+    loadingText: 'Cargando',
+    noDataText: 'Sin datos',
+    pageText: '',
+    ofText: 'de',
+    rowsText: 'registros',
+    pageSizeOptions: [10, 20, 25, 50, 100],
+};
