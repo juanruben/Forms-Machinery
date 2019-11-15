5a9af7b3be9b659234c66c15c387b506720b70b8
juanruben <juanruben@gmail.com>
2019-11-15 12:01:49 -0300

    Crea componente Select

---
+++ src/Components/Select/Select.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Select.scss';
+
+const Select = (props) => {
+    const {
+        label, options, errors, name, value, onChange, placeholder, readOnly, hideReadOnly,
+    } = props;
+    const warning = (errors.length > 0);
+
+    if (hideReadOnly && readOnly) {
+        return null;
+    }
+
+    if (readOnly) {
+        return (
+            <>
+                <div className="bold-label">{label}</div>
+                <div className="read-only-container">
+                    <div className="read-only-value">{value}</div>
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <>
+            {label}
+            <div className="input-bordered-container">
+                <select name={name} id="" className="select-icafal" defaultValue="0" onChange={onChange}>
+                    <option value="0" disabled>{placeholder}</option>
+                    {options.map((item) => (
+                        <option value={item.id}>{item.name}</option>
+                    ))}
+                </select>
+                {warning && <i className="fas fa-exclamation warning" />}
+            </div>
+        </>
+    );
+};
+
+Select.propTypes = {
+    label: PropTypes.string,
+    errors: PropTypes.string,
+    options: PropTypes.array,
+    name: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.node,
+    placeholder: PropTypes.string,
+    readOnly: PropTypes.bool,
+    hideReadOnly: PropTypes.bool,
+};
+
+Select.defaultProps = {
+    label: '',
+    errors: '',
+    options: [],
+    value: '',
+    placeholder: '',
+    readOnly: false,
+    hideReadOnly: false,
+};
+
+export default Select;
---
+++ src/Components/Select/Select.test.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Select from './Select';
+
+describe('Input', () => {
+    let wrapper;
+    let callback;
+    beforeEach(() => {
+        callback = jest.fn();
+        wrapper = shallow(<Select name="test" onChange={callback} />);
+    });
+
+    it('should render correctly', () => expect(wrapper.debug()).toMatchSnapshot());
+
+    it('value check', () => {
+        wrapper.find('input').simulate('change', { target: { value: 'test' } });
+        expect(callback).toHaveBeenCalled();
+    });
+
+    it('should render text', () => {
+        wrapper.setProps({ value: 'test' });
+        expect(wrapper.find('input').prop('value')).toEqual('test');
+    });
+
+    it('should render icon', () => {
+        wrapper.setProps({ icon: 'icon' });
+        expect(wrapper.find('i')).toHaveLength(1);
+    });
+
+    it('should render error correctly', () => {
+        wrapper.setProps({ errors: 'test' });
+        expect(wrapper.find('i')).toHaveLength(1);
+        expect(wrapper.find('input').hasClass('border-error')).toBe(true);
+    });
+
+    it('should render input', () => {
+        expect(wrapper.find('input')).toHaveLength(1);
+    });
+
+    it('should render div', () => {
+        expect(wrapper.find('div')).toHaveLength(1);
+    });
+});
