2494a81664db1db4c5a8b031ef09591da92c9048
juanruben <juanruben@gmail.com>
2019-11-14 22:52:54 -0300

    Pasa data y locked a formulario

--- src/Pages/Clients/Clients.jsx
+++ src/Pages/Clients/Clients.jsx
@@ -15,7 +15,10 @@ class Clients extends Component {
             showConfirm: false,
         };
         this.handleRemove = this.handleRemove.bind(this);
+        this.findData = this.findData.bind(this);
     }
+
+    findData = (id) => dummyData.find((item) => item.id === id);
 
     handleRemove() {
         this.setState({ showConfirm: true });
@@ -29,7 +32,7 @@ class Clients extends Component {
                 accessor: 'name',
                 Cell: (row) => (
                     <ModalView title={row.original.name}>
-                        <ClientForm />
+                        <ClientForm data={this.findData(row.original.id)} locked />
                     </ModalView>
                 ),
             },
@@ -59,7 +62,7 @@ class Clients extends Component {
                 Cell: (row) => (
                     <div className="form-actions">
                         <ModalView title="Editar cliente" type="edit">
-                            <ClientForm />
+                            <ClientForm data={this.findData(row.original.id)} />
                         </ModalView>
                         <span className="form-actions__icon" onClick={this.handleRemove}><i className="fas fa-trash" /></span>
                     </div>
