fbafa33b5f9867ddd078ff858ece8d4d41a4ee70
juanruben <juanruben@gmail.com>
2019-11-15 20:51:53 -0300

    Crea gestion de loading a través del contexto

--- src/Pages/Clients/ClientForm.jsx
+++ src/Pages/Clients/ClientForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'reactstrap';
+import { StateContext } from '../../State';
 import Input from '../../Components/Input/Input';
 import Button from '../../Components/Button/Button';
 
@@ -24,16 +25,6 @@ class ClientForm extends Component {
         }
     }
 
-    async handleNew() {
-        const { callback } = this.props;
-        callback();
-    }
-
-    async handleUpdate() {
-        const { callback } = this.props;
-        callback();
-    }
-
     onChange(event) {
         const { name, value } = event.target;
         this.setState({
@@ -42,16 +33,42 @@ class ClientForm extends Component {
         });
     }
 
+    async handleNew() {
+        this.toggleLoading(true);
+        const { callback } = this.props;
+        setTimeout(() => {
+            this.toggleLoading(false);
+            callback();
+        }, 500);
+    }
+
+    async handleUpdate() {
+        this.toggleLoading(true);
+        const { callback } = this.props;
+        setTimeout(() => {
+            this.toggleLoading(false);
+            callback();
+        }, 500);
+    }
+
+    toggleLoading(value) {
+        const [, dispatch] = this.context;
+        dispatch({
+            type: 'SET_LOADING',
+            value,
+        });
+    }
+
 
     render() {
         const { readOnly } = this.props;
-        const { createMode, name, business_name, rut, contact, phone, email, address } = this.state;
+        const { createMode, name, company, rut, contact, phone, email, address } = this.state;
 
         return (
             <>
                 <Row>
                     <Col md={6}><Input name="name" onChange={this.onChange} label="Nombre empresa" placeholder="Nombre empresa" value={name} readOnly={readOnly} icon="fas fa-industry" /></Col>
-                    <Col md={6}><Input name="business_name" onChange={this.onChange} label="Razón social" placeholder="Razón social" value={business_name} readOnly={readOnly} /></Col>
+                    <Col md={6}><Input name="company" onChange={this.onChange} label="Razón social" placeholder="Razón social" value={company} readOnly={readOnly} /></Col>
                     <Col md={6}><Input name="rut" onChange={this.onChange} label="Rut empresa" placeholder="Rut empresa" value={rut} readOnly={readOnly} icon="far fa-address-card" /></Col>
                     <Col md={6}><Input name="contact" onChange={this.onChange} label="Contacto" placeholder="Persona contacto" value={contact} readOnly={readOnly} icon="fas fa-user-tie" /></Col>
                     <Col md={6}><Input name="phone" onChange={this.onChange} label="Teléfono de contacto" placeholder="Teléfono de contacto" value={phone} readOnly={readOnly} icon="fas fa-phone" /></Col>
@@ -86,5 +103,7 @@ ClientForm.defaultProps = {
     data: null,
     readOnly: false,
 };
+
+ClientForm.contextType = StateContext;
 
 export default ClientForm;
