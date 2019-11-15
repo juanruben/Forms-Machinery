e228bace41b67d8d283c36805dcbf4ea6cb95eda
juanruben <juanruben@gmail.com>
2019-11-14 22:52:02 -0300

    Formulario de cliente recibe props data y locked

--- src/Pages/Clients/ClientForm.jsx
+++ src/Pages/Clients/ClientForm.jsx
@@ -8,28 +8,58 @@ class ClientForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            data: {},
+            createMode: true,
         };
+        this.handleNew = this.handleNew.bind(this);
+    }
+
+    componentDidMount() {
+        const { data } = this.props;
+        if (data) {
+            this.setState({
+                data,
+                createMode: false,
+            });
+        }
+    }
+
+    async handleNew() {
+        const { callback } = this.props;
+        callback();
+    }
+
+    async handleUpdate() {
+        const { callback } = this.props;
+        callback();
     }
 
     render() {
-        const { callback } = this.props;
+        const { locked } = this.props;
+        const { data, createMode } = this.state;
+
         return (
             <>
                 <Row>
-                    <Col md={6}><Input label="Nombre empresa" placeholder="Nombre empresa" /></Col>
-                    <Col md={6}><Input label="Razón social" placeholder="Razón social" /></Col>
-                    <Col md={6}><Input label="Rut empresa" placeholder="Rut empresa" /></Col>
-                    <Col md={6}><Input label="Teléfono de contacto" placeholder="Teléfono de contacto" /></Col>
-                    <Col md={12}><Input label="Dirección" placeholder="Dirección" /></Col>
-                    <Col md={12}><Input label="Emails" placeholder="correo1@ejemplo.com, correo2@ejemplo.com, ..." /></Col>
+                    <Col md={6}><Input name="name" onChange={() => {}} label="Nombre empresa" placeholder="Nombre empresa" value={data.name} locked={locked} /></Col>
+                    <Col md={6}><Input name="name" onChange={() => {}} label="Razón social" placeholder="Razón social" value={data.name} locked={locked} /></Col>
+                    <Col md={6}><Input name="name" onChange={() => {}} label="Rut empresa" placeholder="Rut empresa" value={data.rut} locked={locked} /></Col>
+                    <Col md={6}><Input name="name" onChange={() => {}} label="Teléfono de contacto" placeholder="Teléfono de contacto" value={data.phone} locked={locked} /></Col>
+                    <Col md={12}><Input name="name" onChange={() => {}} label="Dirección" placeholder="Dirección" value={data.address} locked={locked} /></Col>
+                    <Col md={12}><Input name="name" onChange={() => {}} label="Emails" placeholder="correo1@ejemplo.com, correo2@ejemplo.com, ..." value={data.email} locked={locked} /></Col>
                 </Row>
-                * Estas direcciones de correo recibirán reporte y estado de las maquinarias cuando esta salgan del taller hacia la obra del cliente
-                <Row>
-                    <Col md={8} />
-                    <Col md={4}>
-                        <Button text="Crear" onClick={callback} />
-                    </Col>
-                </Row>
+                {!locked && (
+                    <>
+                        * Estas direcciones de correo recibirán reporte y estado de las maquinarias
+                        cuando esta salgan del taller hacia la obra del cliente
+                        <Row>
+                            <Col md={8} />
+                            <Col md={4}>
+                                <Button text={createMode ? 'Crear' : 'Actualizar'} onClick={this.handleNew} />
+                            </Col>
+                        </Row>
+                    </>
+                )}
             </>
         );
     }
@@ -37,10 +67,14 @@ class ClientForm extends Component {
 
 ClientForm.propTypes = {
     callback: PropTypes.func,
+    data: PropTypes.object,
+    locked: PropTypes.bool,
 };
 
 ClientForm.defaultProps = {
     callback: null,
+    data: null,
+    locked: false,
 };
 
 export default ClientForm;
