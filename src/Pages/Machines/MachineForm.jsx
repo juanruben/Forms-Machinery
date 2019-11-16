455dd45f9fd5dc0a254326d56d21736931037ae0
juanruben <juanruben@gmail.com>
2019-11-15 21:07:34 -0300

    Gestiona loading y estado del formulario

--- src/Pages/Machines/MachineForm.jsx
+++ src/Pages/Machines/MachineForm.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'reactstrap';
+import { StateContext } from '../../State';
 import Input from '../../Components/Input/Input';
 import Button from '../../Components/Button/Button';
 import Select from '../../Components/Select/Select';
+
+const forms = [
+    {
+        id: 1,
+        name: 'Formulario 1',
+    },
+    {
+        id: 2,
+        name: 'Formulario 2',
+    },
+    {
+        id: 3,
+        name: 'Formulario 3',
+    },
+];
+
 
 class MachineForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: {},
             createMode: true,
         };
         this.handleNew = this.handleNew.bind(this);
@@ -19,49 +35,60 @@ class MachineForm extends Component {
         const { data } = this.props;
         if (data) {
             this.setState({
-                data,
+                ...data,
                 createMode: false,
             });
         }
     }
 
+    onChange(event) {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value,
+            errors: {},
+        });
+    }
+
     async handleNew() {
+        this.toggleLoading(true);
         const { callback } = this.props;
-        callback();
+        setTimeout(() => {
+            this.toggleLoading(false);
+            callback();
+        }, 500);
     }
 
     async handleUpdate() {
+        this.toggleLoading(true);
         const { callback } = this.props;
-        callback();
+        setTimeout(() => {
+            this.toggleLoading(false);
+            callback();
+        }, 500);
+    }
+
+    toggleLoading(value) {
+        const [, dispatch] = this.context;
+        dispatch({
+            type: 'SET_LOADING',
+            value,
+        });
     }
 
     render() {
         const { readOnly } = this.props;
-        const { data, createMode } = this.state;
-        const forms = [
-            {
-                id: 1,
-                name: 'Formulario 1',
-            },
-            {
-                id: 2,
-                name: 'Formulario 2',
-            },
-            {
-                id: 3,
-                name: 'Formulario 3',
-            },
-        ];
+        const { createMode, code, company, plate, model, year, machine } = this.state;
+
         return (
             <>
                 <Row>
-                    <Col md={12}><Input label="Nombre" placeholder="Nombre" name="name" onChange={() => { }} value={data.machine} readOnly={readOnly} /></Col>
-                    <Col md={6}><Input label="Código" placeholder="Código" name="name" onChange={() => { }} value={data.code} readOnly={readOnly} /></Col>
-                    <Col md={6}><Input label="Patente" placeholder="Patente" name="name" onChange={() => { }} value={data.plate} readOnly={readOnly} /></Col>
-                    <Col md={6}><Input label="Marca" placeholder="Marca" name="name" onChange={() => { }} value={data.company} readOnly={readOnly} /></Col>
-                    <Col md={6}><Input label="Modelo" placeholder="Modelo" name="name" onChange={() => { }} value={data.model} readOnly={readOnly} /></Col>
-                    <Col md={6}><Input label="Año" placeholder="Año" name="name" onChange={() => { }} value={data.code} readOnly={readOnly} /></Col>
-                    <Col md={6}><Select label="Formulario" options={forms} placeholder="Seleccione..." name="name" onChange={() => { }} value={data.code} readOnly={readOnly} /></Col>
+                    <Col md={12}><Input label="Nombre" placeholder="Nombre" name="company" onChange={() => { }} value={machine} readOnly={readOnly} /></Col>
+                    <Col md={6}><Input label="Código" placeholder="Código" name="code" onChange={() => { }} value={code} readOnly={readOnly} /></Col>
+                    <Col md={6}><Input label="Patente" placeholder="Patente" name="plate" onChange={() => { }} value={plate} readOnly={readOnly} /></Col>
+                    <Col md={6}><Input label="Marca" placeholder="Marca" name="brand" onChange={() => { }} value={company} readOnly={readOnly} /></Col>
+                    <Col md={6}><Input label="Modelo" placeholder="Modelo" name="model" onChange={() => { }} value={model} readOnly={readOnly} /></Col>
+                    <Col md={6}><Input label="Año" placeholder="Año" name="year" onChange={() => { }} value={year} readOnly={readOnly} /></Col>
+                    <Col md={6}><Select label="Formulario" options={forms} placeholder="Seleccione..." name="name" onChange={() => { }} value={code} readOnly={readOnly} /></Col>
                 </Row>
                 {!readOnly && (
                     <Row>
@@ -87,5 +114,7 @@ MachineForm.defaultProps = {
     data: null,
     readOnly: false,
 };
+
+MachineForm.contextType = StateContext;
 
 export default MachineForm;
--- src/Pages/Users/UserForm.jsx
+++ src/Pages/Users/UserForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'reactstrap';
+import { StateContext } from '../../State';
 import Input from '../../Components/Input/Input';
 import Button from '../../Components/Button/Button';
 import Select from '../../Components/Select/Select';
@@ -20,7 +21,6 @@ class UserForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: {},
             createMode: true,
         };
         this.handleNew = this.handleNew.bind(this);
@@ -30,38 +30,62 @@ class UserForm extends Component {
         const { data } = this.props;
         if (data) {
             this.setState({
-                data,
+                ...data,
                 createMode: false,
             });
         }
     }
 
+    onChange(event) {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value,
+            errors: {},
+        });
+    }
+
     async handleNew() {
+        this.toggleLoading(true);
         const { callback } = this.props;
-        callback();
+        setTimeout(() => {
+            this.toggleLoading(false);
+            callback();
+        }, 500);
     }
 
     async handleUpdate() {
+        this.toggleLoading(true);
         const { callback } = this.props;
-        callback();
+        setTimeout(() => {
+            this.toggleLoading(false);
+            callback();
+        }, 500);
+    }
+
+    toggleLoading(value) {
+        const [, dispatch] = this.context;
+        dispatch({
+            type: 'SET_LOADING',
+            value,
+        });
     }
 
     render() {
         const { readOnly } = this.props;
-        const { data, createMode } = this.state;
+        const { createMode, name, company, model, rut, email, profile, phone, password, repeatPassword } = this.state;
 
         return (
             <>
                 <Row>
-                    <Col md={6}><Input name="name" value={data.name} onChange={() => { }} readOnly={readOnly} label="Nombre" placeholder="Nombre" icon="fas fa-user-alt" /></Col>
-                    <Col md={6}><Input name="name" value={data.name} onChange={() => { }} readOnly={readOnly} label="Apellido" placeholder="Apellido" icon="fas fa-user-alt" /></Col>
-                    <Col md={6}><Input name="name" value={data.name} onChange={() => { }} readOnly={readOnly} label="Nombre de usuario" placeholder="Nombre de usuario" icon="fas fa-user-alt" /></Col>
-                    <Col md={6}><Input name="name" value={data.rut} onChange={() => { }} readOnly={readOnly} label="RUT" placeholder="RUT" icon="far fa-address-card" /></Col>
-                    <Col md={6}><Input name="name" value={data.phone} onChange={() => { }} readOnly={readOnly} label="Teléfono" placeholder="Teléfono" icon="fas fa-phone" /></Col>
-                    <Col md={6}><Input name="name" value={data.email} onChange={() => { }} readOnly={readOnly} label="Email" placeholder="Email" icon="far fa-envelope" /></Col>
-                    <Col md={12}><Select label="Rol" value={data.profile} options={profiles} placeholder="Seleccione..." name="name" onChange={() => { }} readOnly={readOnly} /></Col>
-                    <Col md={6}><Input name="name" onChange={() => { }} readOnly={readOnly} hideReadOnly label="Contraseña" placeholder="Contraseña" /></Col>
-                    <Col md={6}><Input name="name" onChange={() => { }} readOnly={readOnly} hideReadOnly label="Repita contraseña" placeholder="Repita contraseña" /></Col>
+                    <Col md={6}><Input name="name" value={name} onChange={() => { }} readOnly={readOnly} label="Nombre" placeholder="Nombre" icon="fas fa-user-alt" /></Col>
+                    <Col md={6}><Input name="company" value={company} onChange={() => { }} readOnly={readOnly} label="Apellido" placeholder="Apellido" icon="fas fa-user-alt" /></Col>
+                    <Col md={6}><Input name="model" value={model} onChange={() => { }} readOnly={readOnly} label="Nombre de usuario" placeholder="Nombre de usuario" icon="fas fa-user-alt" /></Col>
+                    <Col md={6}><Input name="rut" value={rut} onChange={() => { }} readOnly={readOnly} label="RUT" placeholder="RUT" icon="far fa-address-card" /></Col>
+                    <Col md={6}><Input name="phone" value={phone} onChange={() => { }} readOnly={readOnly} label="Teléfono" placeholder="Teléfono" icon="fas fa-phone" /></Col>
+                    <Col md={6}><Input name="email" value={email} onChange={() => { }} readOnly={readOnly} label="Email" placeholder="Email" icon="far fa-envelope" /></Col>
+                    <Col md={12}><Select label="Rol" value={profile} options={profiles} placeholder="Seleccione..." name="name" onChange={() => { }} readOnly={readOnly} /></Col>
+                    <Col md={6}><Input name="password" value={password} onChange={() => { }} readOnly={readOnly} hideReadOnly label="Contraseña" placeholder="Contraseña" /></Col>
+                    <Col md={6}><Input name="repeatPassword" value={repeatPassword} onChange={() => { }} readOnly={readOnly} hideReadOnly label="Repita contraseña" placeholder="Repita contraseña" /></Col>
                 </Row>
                 {!readOnly && (
                     <Row>
@@ -87,5 +111,7 @@ UserForm.defaultProps = {
     data: null,
     readOnly: false,
 };
+
+UserForm.contextType = StateContext;
 
 export default UserForm;
