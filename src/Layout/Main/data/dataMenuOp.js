47b9d85c38170a3e471866e13741e9a2d2a7f9cc
juanruben <juanruben@gmail.com>
2019-11-18 11:11:47 -0300

    Agrega iconos

--- src/Layout/Main/data/dataMenuOp.js
+++ src/Layout/Main/data/dataMenuOp.js
@@ -3,10 +3,12 @@ export default [
         id: 1,
         title: 'Registrar Entrada',
         path: '/entrada',
+        icon: 'fas fa-sign-in-alt',
     },
     {
         id: 2,
         title: 'Registrar Salida',
         path: '/salida',
+        icon: 'fas fa-sign-out-alt',
     },
 ];
