a834631bba108f2786491f007540f0ef2cf43e6c
juanruben <juanruben@gmail.com>
2019-11-19 12:14:07 -0300

    Ordena identación

--- src/Pages/Clients/Clients.jsx
+++ src/Pages/Clients/Clients.jsx
@@ -59,7 +59,9 @@ class Clients extends Component {
                 Header: 'Email',
                 accessor: 'email',
                 Cell: (row) => (
-                    <a href={`mailto:${row.original.email}`}>{row.original.email}}</a>
+                    <a href={`mailto:${row.original.email}`}>
+                        {row.original.email}
+                    </a>
                 ),
                 filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: ['email'] }),
                 filterAll: true,
