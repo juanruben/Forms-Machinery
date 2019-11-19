8a4ba0d091efaaad371372c2de501358f64870dc
juanruben <juanruben@gmail.com>
2019-11-19 17:01:08 -0300

    Valida datos del formulario

--- src/Pages/Users/UserForm.jsx
+++ src/Pages/Users/UserForm.jsx
@@ -22,15 +22,18 @@ class UserForm extends Component {
         super(props);
         this.state = {
             createMode: true,
+            data: {},
+            errors: {},
         };
         this.handleNew = this.handleNew.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
     componentDidMount() {
         const { data } = this.props;
         if (data) {
             this.setState({
-                ...data,
+                data,
                 createMode: false,
             });
         }
@@ -38,10 +41,63 @@ class UserForm extends Component {
 
     onChange(event) {
         const { name, value } = event.target;
+        const { data, errors } = this.state;
+        data[name] = value;
+        errors[name] = '';
         this.setState({
-            [name]: value,
-            errors: {},
+            data,
+            errors,
         });
+    }
+
+    validForm() {
+        const { data } = this.state;
+        const {
+            name, business_name, rut, contact, phone, email, address,
+        } = data;
+        const errors = {};
+        let formIsValid = true;
+
+        if (!name || name.trim().length === 0) {
+            formIsValid = false;
+            errors.name = 'Requerido';
+        }
+
+        if (!business_name || business_name.trim().length === 0) {
+            formIsValid = false;
+            errors.business_name = 'Requerido';
+        }
+
+        if (!rut || rut.trim().length === 0) {
+            formIsValid = false;
+            errors.rut = 'Requerido';
+        }
+
+        if (!contact || contact.trim().length === 0) {
+            formIsValid = false;
+            errors.contact = 'Requerido';
+        }
+
+        if (!phone || contact.trim().length === 0) {
+            formIsValid = false;
+            errors.phone = 'Requerido';
+        }
+
+        if (!email || email.trim().length === 0) {
+            formIsValid = false;
+            errors.email = 'Requerido';
+        }
+
+        if (!address || address.trim().length === 0) {
+            formIsValid = false;
+            errors.address = 'Requerido';
+        }
+
+        this.setState({
+            errors,
+        });
+
+        return formIsValid;
     }
 
     async handleNew() {
@@ -72,20 +128,23 @@ class UserForm extends Component {
 
     render() {
         const { readOnly } = this.props;
-        const { createMode, name, company, model, rut, email, profile, phone, password, repeatPassword } = this.state;
+        const { createMode, errors, data } = this.state;
+        const {
+            name, last_name, username, rut, phone, email, role_id, password, repeatPassword,
+        } = data;
 
         return (
             <>
                 <Row>
-                    <Col md={6}><Input name="name" value={name} onChange={() => { }} readOnly={readOnly} label="Nombre" placeholder="Nombre" icon="fas fa-user-alt" /></Col>
-                    <Col md={6}><Input name="company" value={company} onChange={() => { }} readOnly={readOnly} label="Apellido" placeholder="Apellido" icon="fas fa-user-alt" /></Col>
-                    <Col md={6}><Input name="model" value={model} onChange={() => { }} readOnly={readOnly} label="Nombre de usuario" placeholder="Nombre de usuario" icon="fas fa-user-alt" /></Col>
-                    <Col md={6}><Input name="rut" value={rut} onChange={() => { }} readOnly={readOnly} label="RUT" placeholder="RUT" icon="far fa-address-card" /></Col>
-                    <Col md={6}><Input name="phone" value={phone} onChange={() => { }} readOnly={readOnly} label="Teléfono" placeholder="Teléfono" icon="fas fa-phone" /></Col>
-                    <Col md={6}><Input name="email" value={email} onChange={() => { }} readOnly={readOnly} label="Email" placeholder="Email" icon="far fa-envelope" /></Col>
-                    <Col md={12}><Select label="Rol" value={profile} options={profiles} placeholder="Seleccione..." name="name" onChange={() => { }} readOnly={readOnly} /></Col>
-                    <Col md={6}><Input name="password" value={password} onChange={() => { }} readOnly={readOnly} hideReadOnly label="Contraseña" placeholder="Contraseña" /></Col>
-                    <Col md={6}><Input name="repeatPassword" value={repeatPassword} onChange={() => { }} readOnly={readOnly} hideReadOnly label="Repita contraseña" placeholder="Repita contraseña" /></Col>
+                    <Col md={6}><Input name="name" value={name} onChange={this.onChange} readOnly={readOnly} label="Nombre" placeholder="Nombre" icon="fas fa-user-alt" errors={errors} required /></Col>
+                    <Col md={6}><Input name="last_name" value={last_name} onChange={this.onChange} readOnly={readOnly} label="Apellido" placeholder="Apellido" icon="fas fa-user-alt" errors={errors} required /></Col>
+                    <Col md={6}><Input name="username" value={username} onChange={this.onChange} readOnly={readOnly} label="Nombre de usuario" placeholder="Nombre de usuario" icon="fas fa-user-alt" errors={errors} required /></Col>
+                    <Col md={6}><Input name="rut" value={rut} onChange={this.onChange} readOnly={readOnly} label="RUT" placeholder="RUT" icon="far fa-address-card" errors={errors} required /></Col>
+                    <Col md={6}><Input name="phone" value={phone} onChange={this.onChange} readOnly={readOnly} label="Teléfono" placeholder="Teléfono" icon="fas fa-phone" errors={errors} required /></Col>
+                    <Col md={6}><Input name="email" value={email} onChange={this.onChange} readOnly={readOnly} label="Email" placeholder="Email" icon="far fa-envelope" errors={errors} required /></Col>
+                    <Col md={12}><Select label="Rol" value={role_id} options={profiles} placeholder="Seleccione..." name="role_id" onChange={this.onChange} readOnly={readOnly} errors={errors} required /></Col>
+                    <Col md={6}><Input name="password" type="password" value={password} onChange={this.onChange} readOnly={readOnly} hideReadOnly label="Contraseña" placeholder="Contraseña" errors={errors} required /></Col>
+                    <Col md={6}><Input name="repeatPassword" type="password" value={repeatPassword} onChange={this.onChange} readOnly={readOnly} hideReadOnly label="Repita contraseña" placeholder="Repita contraseña" errors={errors} required /></Col>
                 </Row>
                 {!readOnly && (
                     <Row>
