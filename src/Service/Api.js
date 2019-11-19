84aa86bdbe723688d313eef908cec977020ca72e
juanruben <juanruben@gmail.com>
2019-11-19 16:59:54 -0300

    Agrega globalmente token a los headers

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -1,4 +1,17 @@
 import axios from 'axios';
+import { decode } from './Utils';
+
+axios.interceptors.request.use((config) => {
+    const tokenSession = localStorage.getItem('t');
+
+    if (tokenSession) {
+        const token = decode(tokenSession);
+        // eslint-disable-next-line no-param-reassign
+        config.headers.Authorization = token;
+    }
+
+    return config;
+}, (err) => Promise.reject(err));
 
 export async function login(user, password) {
     const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, {
