db9bb1bec6c3975d7064eb38ebed30c1a8113d56
juanruben <juanruben@gmail.com>
2019-11-19 12:14:36 -0300

    Agrega validación

--- src/Pages/Clients/ClientForm.jsx
+++ src/Pages/Clients/ClientForm.jsx
@@ -10,16 +10,19 @@ class ClientForm extends Component {
         super(props);
         this.state = {
             createMode: true,
+            data: {},
+            errors: {},
         };
         this.handleNew = this.handleNew.bind(this);
         this.onChange = this.onChange.bind(this);
+        this.validForm = this.validForm.bind(this);
     }
 
     componentDidMount() {
         const { data } = this.props;
         if (data) {
             this.setState({
-                ...data,
+                data,
                 createMode: false,
             });
         }
@@ -27,28 +30,87 @@ class ClientForm extends Component {
 
     onChange(event) {
         const { name, value } = event.target;
+        const { data, errors } = this.state;
+        data[name] = value;
+        errors[name] = '';
         this.setState({
-            [name]: value,
-            errors: {},
+            data,
+            errors,
         });
     }
 
+    validForm() {
+        const { data } = this.state;
+        const {
+            name, business_name, rut, contact, phone, email, address,
+        } = data;
+        const errors = {};
+        let formIsValid = true;
+
+        if (!name || name.trim().length === 0) {
+            formIsValid = false;
+            errors.name = 'Requerido';
+        }
+
+        if (!business_name || business_name.trim().length === 0) {
+            formIsValid = false;
+            errors.business_name = 'Requerido';
+        }
+
+        if (!rut || rut.trim().length === 0) {
+            formIsValid = false;
+            errors.rut = 'Requerido';
+        }
+
+        if (!contact || contact.trim().length === 0) {
+            formIsValid = false;
+            errors.contact = 'Requerido';
+        }
+
+        if (!phone || contact.trim().length === 0) {
+            formIsValid = false;
+            errors.phone = 'Requerido';
+        }
+
+        if (!email || email.trim().length === 0) {
+            formIsValid = false;
+            errors.email = 'Requerido';
+        }
+
+        if (!address || address.trim().length === 0) {
+            formIsValid = false;
+            errors.address = 'Requerido';
+        }
+
+        this.setState({
+            errors,
+        });
+
+        return formIsValid;
+    }
+
     async handleNew() {
-        this.toggleLoading(true);
-        const { callback } = this.props;
-        setTimeout(() => {
-            this.toggleLoading(false);
-            callback();
-        }, 500);
+        if (this.validForm()) {
+            this.toggleLoading(true);
+            const { callback } = this.props;
+
+            setTimeout(() => {
+                this.toggleLoading(false);
+                callback();
+            }, 500);
+        }
     }
 
     async handleUpdate() {
-        this.toggleLoading(true);
-        const { callback } = this.props;
-        setTimeout(() => {
-            this.toggleLoading(false);
-            callback();
-        }, 500);
+        if (this.validForm()) {
+            this.toggleLoading(true);
+            const { callback } = this.props;
+
+            setTimeout(() => {
+                this.toggleLoading(false);
+                callback();
+            }, 500);
+        }
     }
 
     toggleLoading(value) {
@@ -62,18 +124,22 @@ class ClientForm extends Component {
 
     render() {
         const { readOnly } = this.props;
-        const { createMode, name, company, rut, contact, phone, email, address } = this.state;
+        const { createMode, errors, data } = this.state;
+        const {
+            name, business_name, rut, contact, phone, email, address,
+        } = data;
+
 
         return (
             <>
                 <Row>
-                    <Col md={6}><Input name="name" onChange={this.onChange} label="Nombre empresa" placeholder="Nombre empresa" value={name} readOnly={readOnly} icon="fas fa-industry" /></Col>
-                    <Col md={6}><Input name="company" onChange={this.onChange} label="Razón social" placeholder="Razón social" value={company} readOnly={readOnly} /></Col>
-                    <Col md={6}><Input name="rut" onChange={this.onChange} label="Rut empresa" placeholder="Rut empresa" value={rut} readOnly={readOnly} icon="far fa-address-card" /></Col>
-                    <Col md={6}><Input name="contact" onChange={this.onChange} label="Contacto" placeholder="Persona contacto" value={contact} readOnly={readOnly} icon="fas fa-user-tie" /></Col>
-                    <Col md={6}><Input name="phone" onChange={this.onChange} label="Teléfono de contacto" placeholder="Teléfono de contacto" value={phone} readOnly={readOnly} icon="fas fa-phone" /></Col>
-                    <Col md={6}><Input name="address" onChange={this.onChange} label="Dirección" placeholder="Dirección" value={address} readOnly={readOnly} icon="fas fa-map-marked-alt" /></Col>
-                    <Col md={12}><Input name="email" onChange={this.onChange} label="Emails" placeholder="correo1@ejemplo.com, correo2@ejemplo.com, ..." value={email} readOnly={readOnly} icon="far fa-envelope" /></Col>
+                    <Col md={6}><Input name="name" onChange={this.onChange} label="Nombre empresa" placeholder="Nombre empresa" value={name} readOnly={readOnly || !createMode} icon="fas fa-industry" errors={errors} required /></Col>
+                    <Col md={6}><Input name="business_name" onChange={this.onChange} label="Razón social" placeholder="Razón social" value={business_name} readOnly={readOnly || !createMode} errors={errors} required /></Col>
+                    <Col md={6}><Input name="rut" onChange={this.onChange} label="Rut empresa" placeholder="Rut empresa" value={rut} readOnly={readOnly || !createMode} icon="far fa-address-card" errors={errors} required /></Col>
+                    <Col md={6}><Input name="contact" onChange={this.onChange} label="Contacto" placeholder="Persona contacto" value={contact} readOnly={readOnly} icon="fas fa-user-tie" errors={errors} required /></Col>
+                    <Col md={6}><Input name="phone" onChange={this.onChange} label="Teléfono de contacto" placeholder="Teléfono de contacto" value={phone} readOnly={readOnly} icon="fas fa-phone" errors={errors} required /></Col>
+                    <Col md={6}><Input name="address" onChange={this.onChange} label="Dirección" placeholder="Dirección" value={address} readOnly={readOnly} icon="fas fa-map-marked-alt" errors={errors} required /></Col>
+                    <Col md={12}><Input name="email" onChange={this.onChange} label="Emails" placeholder="correo1@ejemplo.com, correo2@ejemplo.com, ..." value={email} readOnly={readOnly} icon="far fa-envelope" errors={errors} required /></Col>
                 </Row>
                 {!readOnly && (
                     <>
