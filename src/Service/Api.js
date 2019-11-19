23171234334008afe4880498a68aac15af50f9fb
juanruben <juanruben@gmail.com>
2019-11-19 12:24:43 -0300

    Refactoriza para capturar status code

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
 export async function login(user, password) {
-    try {
-        return await axios.post(`${process.env.REACT_APP_API_URL}/login`, {
-            user,
-            password,
-        }).then((res) => res);
-    } catch (e) {
-        return null;
-    }
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/login`, {
+        user,
+        password,
+    }).then((res) => res)
+        .catch((error) => {
+            if (error.response) {
+                return error.response;
+            }
+            return null;
+        });
+    return response;
 }
