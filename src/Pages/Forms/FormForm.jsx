7d2f87c82b592669335c8840dd3836c0c2c57514
juanruben <juanruben@gmail.com>
2019-11-20 13:45:57 -0300

    Gestiona actualización

--- src/Pages/Forms/FormForm.jsx
+++ src/Pages/Forms/FormForm.jsx
@@ -1,25 +1,120 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'reactstrap';
+import { Spinner } from 'reactstrap';
 import Input from '../../Components/Input/Input';
 import Button from '../../Components/Button/Button';
+import { addForm, updateForm } from '../../Service/Api';
 
 class NewForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            createMode: true,
+            data: {},
+            errors: {},
         };
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.validForm = this.validForm.bind(this);
+    }
+
+    componentDidMount() {
+        const { data } = this.props;
+        if (data) {
+            this.setState({
+                data,
+                createMode: false,
+            });
+        }
+    }
+
+    validForm() {
+        const { data } = this.state;
+        const { name } = data;
+        const errors = {};
+        let formIsValid = true;
+
+        if (!name || name.trim().length === 0) {
+            formIsValid = false;
+            errors.name = 'Requerido';
+        }
+
+        this.setState({
+            errors,
+        });
+
+        return formIsValid;
+    }
+
+    onChange(event) {
+        const { name, value } = event.target;
+        const { data, errors } = this.state;
+        data[name] = value;
+        errors[name] = '';
+        this.setState({
+            data,
+            errors,
+        });
+    }
+
+    async handleCreate() {
+        if (this.validForm()) {
+            this.toggleLoading(true);
+            const { data } = this.state;
+
+            await addForm(data).then((response) => {
+                if (response && response.status === 201) {
+                    const { callback } = this.props;
+                    callback();
+                }
+            }).catch((error) => {
+                this.setState({
+                    errors: error.response.data.errors,
+                });
+            });
+
+            this.toggleLoading(false);
+        }
+    }
+
+    async handleUpdate() {
+        if (this.validForm()) {
+            this.toggleLoading(true);
+            const { data } = this.state;
+
+            await updateForm(data).then((response) => {
+                if (response && response.status === 200) {
+                    const { callback } = this.props;
+                    callback();
+                }
+            }).catch((error) => {
+                this.setState({
+                    errors: error.response.data.errors,
+                });
+            });
+            this.toggleLoading(false);
+        }
+    }
+
+    toggleLoading(value) {
+        this.setState({
+            loading: value,
+        });
     }
 
     render() {
+        const {
+            createMode, errors, data, loading,
+        } = this.state;
+        const { name } = data;
+
         return (
             <>
-                <Input label="Nombre" placeholder="Nombre" />
-                <Row>
-                    <Col md={8} />
-                    <Col md={4}>
-                        <Button text="Crear" />
-                    </Col>
-                </Row>
+                <Input name="name" onChange={this.onChange} label="Nombre" placeholder="Nombre de formulario" value={name} errors={errors} required />
+                <div className="form-footer">
+                    {loading && <div className="spinner"><Spinner /></div>}
+                    <Button text={createMode ? 'Crear' : 'Actualizar'} onClick={createMode ? this.handleCreate : this.handleUpdate} />
+                </div>
             </>
         );
     }
