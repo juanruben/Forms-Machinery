bb999ff4af6c537be10cd88335e3b4ca38253cb9
juanruben <juanruben@gmail.com>
2019-11-20 10:59:58 -0300

    Agrega tests de funciones nuevas

--- src/Service/Utils.test.js
+++ src/Service/Utils.test.js
@@ -1,4 +1,7 @@
-import { validateEmail, validateUsername, validatePassword, encode, decode } from './Utils';
+import {
+    validateEmail, validateUsername, validatePassword, encode, decode,
+    validateRut, formatRut, unformatRut,
+} from './Utils';
 
 describe('Utils', () => {
     test('validate email', () => {
@@ -34,4 +37,21 @@ describe('Utils', () => {
     test('decoding', () => {
         expect(decode('WkVkV2VtUkJQVDA9')).toEqual('test');
     });
+
+    test('validate rut', () => {
+        expect(validateRut('Abcde1')).toBeFalsy();
+        expect(validateRut('25622766-5')).toBeTruthy();
+        expect(validateRut('')).toBeFalsy();
+    });
+
+    test('format rut', () => {
+        expect(formatRut('256227665')).toEqual('25.622.766-5');
+        expect(formatRut('')).toEqual('');
+    });
+
+    test('unformat rut', () => {
+        expect(unformatRut('25.622.766-5')).toEqual('256227665');
+        expect(unformatRut('')).toEqual('');
+    });
+
 });
