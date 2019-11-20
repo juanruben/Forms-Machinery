be424001290447a3a65fa35b159e256f8a24b4dc
juanruben <juanruben@gmail.com>
2019-11-20 14:27:17 -0300

    Agrega validación y refactoriza

--- src/Pages/Machines/MachineForm.jsx
+++ src/Pages/Machines/MachineForm.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable camelcase */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Spinner } from 'reactstrap';
 import { StateContext } from '../../State';
 import Input from '../../Components/Input/Input';
 import Button from '../../Components/Button/Button';
 import Select from '../../Components/Select/Select';
+import { addMachine, updateMachine } from '../../Service/Api';
 
 const forms = [
     {
@@ -29,8 +31,11 @@ class MachineForm extends Component {
             createMode: true,
             data: {},
             errors: {},
+            loading: false,
         };
-        this.handleNew = this.handleNew.bind(this);
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.onChange = this.onChange.bind(this);
     }
 
     componentDidMount() {
@@ -54,57 +59,131 @@ class MachineForm extends Component {
         });
     }
 
-    async handleNew() {
-        this.toggleLoading(true);
-        const { callback } = this.props;
-        setTimeout(() => {
+    validForm() {
+        const { data } = this.state;
+        const {
+            name, code, plate, model, brand, year, form_id,
+        } = data;
+        const errors = {};
+        let formIsValid = true;
+
+        if (!name || name.trim().length === 0) {
+            formIsValid = false;
+            errors.name = ['Requerido'];
+        }
+
+        if (!code || code.trim().length === 0) {
+            formIsValid = false;
+            errors.code = ['Requerido'];
+        }
+
+        if (!plate || plate.trim().length === 0) {
+            formIsValid = false;
+            errors.plate = ['Requerido'];
+        }
+
+        if (!model || model.trim().length === 0) {
+            formIsValid = false;
+            errors.model = ['Requerido'];
+        }
+
+        if (!brand || brand.trim().length === 0) {
+            formIsValid = false;
+            errors.brand = ['Requerido'];
+        }
+
+        if (!year || year.trim().length === 0) {
+            formIsValid = false;
+            errors.year = ['Requerido'];
+        }
+
+        if (!form_id || form_id === 0) {
+            formIsValid = false;
+            errors.role_id = ['Requerido'];
+        }
+
+        this.setState({
+            errors,
+        });
+
+        return formIsValid;
+    }
+
+    async handleCreate() {
+        if (this.validForm()) {
+            this.toggleLoading(true);
+            const { data } = this.state;
+
+            await addMachine(data).then((response) => {
+                if (response && response.status === 201) {
+                    const { callback } = this.props;
+                    callback();
+                }
+            }).catch((error) => {
+                this.setState({
+                    errors: error.response.data.errors,
+                });
+            });
             this.toggleLoading(false);
-            callback();
-        }, 500);
+        }
     }
 
     async handleUpdate() {
-        this.toggleLoading(true);
-        const { callback } = this.props;
-        setTimeout(() => {
+        if (this.validForm()) {
+            this.toggleLoading(true);
+            const { data } = this.state;
+
+            await updateMachine(data).then((response) => {
+                if (response && response.status === 200) {
+                    const { callback } = this.props;
+                    callback();
+                }
+            }).catch((error) => {
+                this.setState({
+                    errors: error.response.data.errors,
+                });
+            });
             this.toggleLoading(false);
-            callback();
-        }, 500);
+        }
     }
 
     toggleLoading(value) {
-        const [, dispatch] = this.context;
-        dispatch({
-            type: 'SET_LOADING',
-            value,
+        this.setState({
+            loading: value,
         });
     }
 
     render() {
         const { readOnly } = this.props;
-        const { data, createMode, errors } = this.state;
         const {
-            code, company, plate, model, year, machine,
+            data, createMode, errors, loading,
+        } = this.state;
+        const {
+            name, code, plate, model, brand, year, form_id,
         } = data;
+
+        const rest = {
+            onChange: this.onChange,
+            readOnly,
+            errors,
+        };
 
         return (
             <>
                 <Row>
-                    <Col md={12}><Input label="Nombre" placeholder="Nombre" name="company" onChange={() => { }} value={machine} readOnly={readOnly} errors={errors} /></Col>
-                    <Col md={6}><Input label="Código" placeholder="Código" name="code" onChange={() => { }} value={code} readOnly={readOnly} errors={errors} /></Col>
-                    <Col md={6}><Input label="Patente" placeholder="Patente" name="plate" onChange={() => { }} value={plate} readOnly={readOnly} errors={errors} /></Col>
-                    <Col md={6}><Input label="Marca" placeholder="Marca" name="brand" onChange={() => { }} value={company} readOnly={readOnly} errors={errors} /></Col>
-                    <Col md={6}><Input label="Modelo" placeholder="Modelo" name="model" onChange={() => { }} value={model} readOnly={readOnly} errors={errors} /></Col>
-                    <Col md={6}><Input label="Año" placeholder="Año" name="year" onChange={() => { }} value={year} readOnly={readOnly} errors={errors} /></Col>
-                    <Col md={6}><Select label="Formulario" options={forms} placeholder="Seleccione..." name="name" onChange={() => { }} value={code} readOnly={readOnly} errors={errors} /></Col>
+                    <Col md={12}><Input label="Nombre" name="name" value={name} {...rest} /></Col>
+                    <Col md={6}><Input label="Código" name="code" value={code} {...rest} /></Col>
+                    <Col md={6}><Input label="Patente" name="plate" value={plate} {...rest} /></Col>
+                    <Col md={6}><Input label="Modelo" name="model" value={model} {...rest} /></Col>
+                    <Col md={6}><Input label="Marca" name="brand" value={brand} {...rest} /></Col>
+                    <Col md={6}><Input label="Año" name="year" value={year} {...rest} /></Col>
+                    <Col md={6}><Select label="Formulario" options={forms} placeholder="Seleccione..." name="form_id" value={String(form_id)} {...rest} /></Col>
                 </Row>
                 {!readOnly && (
-                    <Row>
-                        <Col md={8} />
-                        <Col md={4}>
-                            <Button text={createMode ? 'Crear' : 'Actualizar'} onClick={this.handleNew} />
-                        </Col>
-                    </Row>
+                    <div className="form-footer">
+                        {loading && <div className="spinner"><Spinner /></div>}
+                        <Button text={createMode ? 'Crear' : 'Actualizar'} onClick={createMode ? this.handleCreate : this.handleUpdate} />
+                    </div>
                 )}
             </>
         );
