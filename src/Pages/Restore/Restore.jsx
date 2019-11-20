7b640b23371cea93ab014b9a4aa03104f6d9ce63
juanruben <juanruben@gmail.com>
2019-11-20 12:33:03 -0300

    Crea vista para reestablecer contraseña

---
+++ src/Pages/Restore/Restore.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { StateContext } from '../../State';
+import { validateEmail } from '../../Service/Utils';
+import LayoutFullWidth from '../../Layout/LayoutFullWidth/LayoutFullWidth';
+import Box from '../../Layout/Box/Box';
+import Logo from '../../Components/Logo/Logo';
+import Input from '../../Components/Input/Input';
+import Button from '../../Components/Button/Button';
+
+import './Restore.scss';
+
+class Restore extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            errors: {},
+            sent: false,
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.validForm = this.validForm.bind(this);
+        this.send = this.send.bind(this);
+    }
+
+    validForm() {
+        const { email } = this.state;
+        const errors = {};
+        let formIsValid = true;
+
+        if (!validateEmail(email)) {
+            formIsValid = false;
+            errors.email = 'Error de formato de email';
+        }
+
+        this.setState({
+            errors,
+        });
+
+        return formIsValid;
+    }
+
+    toggleLoading(value) {
+        const [, dispatch] = this.context;
+        dispatch({
+            type: 'SET_LOADING',
+            value,
+        });
+    }
+
+    async send() {
+        if (this.validForm()) {
+            this.toggleLoading(true);
+            // const { email } = this.state;
+
+            // const response = await LoginApi({
+            //     username,
+            //     password,
+            // });
+            this.setState({
+                sent: true,
+            });
+
+            // if (responseApi.result === 'success') {
+            //     if (responseApi.perfil === 1) {
+            //         window.location.assign('/administrador/');
+            //     } else {
+            //         window.location.assign('/operador/');
+            //     }
+            // }
+
+            this.toggleLoading(false);
+        }
+    }
+
+    handleInputChange(event) {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value,
+            errors: {},
+        });
+    }
+
+    render() {
+        const {
+            email,
+            errors,
+            sent,
+        } = this.state;
+
+        return (
+            <LayoutFullWidth>
+                <Box>
+                    <Logo padding={30} maxWidth={150} />
+                    <div className="login-container">
+                        <Input type="password" name="password" onChange={this.handleInputChange} value={email} icon="far fa-lock" placeholder="Contraseña" errors={errors} />
+                        <Input type="passwordRepeat" name="password" onChange={this.handleInputChange} value={email} icon="far fa-lock" placeholder="Repita contraseña" errors={errors} />
+                        <Button type="button" onClick={this.send} text="Enviar" />
+                    </div>
+                    <Link to="/login" className="link-login">Volver</Link>
+                </Box>
+                {sent && <div className="message-ok">Contraseña actualizada</div>}
+            </LayoutFullWidth>
+        );
+    }
+}
+
+Restore.contextType = StateContext;
+
+export default Restore;
