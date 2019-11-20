026f250da1bd3b1e108c6898e959cf5b61461169
juanruben <juanruben@gmail.com>
2019-11-20 18:36:48 -0300

    Gestiona datos a traves de la api

--- src/Pages/Forms/SectionForm.jsx
+++ src/Pages/Forms/SectionForm.jsx
@@ -1,28 +1,137 @@
+/* eslint-disable camelcase */
 import React, { Component } from 'react';
-import { Row, Col } from 'reactstrap';
+import PropTypes from 'prop-types';
+import { Spinner } from 'reactstrap';
 import Input from '../../Components/Input/Input';
 import Button from '../../Components/Button/Button';
+import { addSection, updateSection } from '../../Service/Api';
 
 class SectionForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            createMode: true,
+            data: {},
+            errors: {},
         };
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.validForm = this.validForm.bind(this);
+    }
+
+    componentDidMount() {
+        const { data } = this.props;
+        if (data) {
+            this.setState({
+                data,
+                createMode: false,
+            });
+        }
+    }
+
+    onChange(event) {
+        const { name, value } = event.target;
+        const { data, errors } = this.state;
+        data[name] = value;
+        errors[name] = '';
+        this.setState({
+            data,
+            errors,
+        });
+    }
+
+    validForm() {
+        const { data } = this.state;
+        const { name } = data;
+        const errors = {};
+        let formIsValid = true;
+
+        if (!name || name.trim().length === 0) {
+            formIsValid = false;
+            errors.name = 'Requerido';
+        }
+
+        this.setState({
+            errors,
+        });
+
+        return formIsValid;
+    }
+
+    async handleCreate() {
+        if (this.validForm()) {
+            this.toggleLoading(true);
+            const { data } = this.state;
+            const { form_id } = this.props;
+
+            await addSection(data, form_id).then((response) => {
+                if (response && response.status === 200) {
+                    const { callback } = this.props;
+                    callback();
+                }
+            }).catch((error) => {
+                this.setState({
+                    errors: error.response.data.errors,
+                });
+            });
+
+            this.toggleLoading(false);
+        }
+    }
+
+    async handleUpdate() {
+        if (this.validForm()) {
+            this.toggleLoading(true);
+            const { data } = this.state;
+
+            await updateSection(data).then((response) => {
+                if (response && response.status === 200) {
+                    const { callback } = this.props;
+                    callback();
+                }
+            }).catch((error) => {
+                this.setState({
+                    errors: error.response.data.errors,
+                });
+            });
+            this.toggleLoading(false);
+        }
+    }
+
+    toggleLoading(value) {
+        this.setState({
+            loading: value,
+        });
     }
 
     render() {
+        const {
+            createMode, errors, data, loading,
+        } = this.state;
+        const { name } = data;
+
         return (
             <>
-                <Input label="Nombre" placeholder="Nombre" />
-                <Row>
-                    <Col md={8} />
-                    <Col md={4}>
-                        <Button text="Crear" />
-                    </Col>
-                </Row>
+                <Input name="name" onChange={this.onChange} label="Nombre" placeholder="Nombre de sección" value={name} errors={errors} required />
+                <div className="form-footer">
+                    {loading && <div className="spinner"><Spinner /></div>}
+                    <Button text={createMode ? 'Crear' : 'Actualizar'} onClick={createMode ? this.handleCreate : this.handleUpdate} />
+                </div>
             </>
         );
     }
 }
+
+SectionForm.propTypes = {
+    callback: PropTypes.func,
+    data: PropTypes.object,
+    form_id: PropTypes.number.isRequired,
+};
+
+SectionForm.defaultProps = {
+    callback: null,
+    data: null,
+};
 
 export default SectionForm;
