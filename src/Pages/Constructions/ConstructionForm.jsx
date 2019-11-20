1aeac7743e70586255a3bd3f5ec3b4c370b9d7f9
juanruben <juanruben@gmail.com>
2019-11-20 16:13:13 -0300

    Carga clientes y ajusta datos de estado

--- src/Pages/Constructions/ConstructionForm.jsx
+++ src/Pages/Constructions/ConstructionForm.jsx
@@ -6,30 +6,52 @@ import { StateContext } from '../../State';
 import Input from '../../Components/Input/Input';
 import Select from '../../Components/Select/Select';
 import Button from '../../Components/Button/Button';
-import { addConstruction, updateConstruction } from '../../Service/Api';
+import { addConstruction, updateConstruction, getClients } from '../../Service/Api';
+
+const status_options = [
+    {
+        id: 'active',
+        name: 'active',
+    },
+    {
+        id: 'finished',
+        name: 'finished',
+    },
+];
 
 class ConstructionForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
             createMode: true,
+            id: null,
             data: {},
             errors: {},
+            clients: [],
         };
         this.handleCreate = this.handleCreate.bind(this);
         this.handleUpdate = this.handleUpdate.bind(this);
         this.onChange = this.onChange.bind(this);
         this.validForm = this.validForm.bind(this);
+        this.loadClients = this.loadClients.bind(this);
     }
 
     componentDidMount() {
         const { data } = this.props;
         if (data) {
             this.setState({
-                data,
+                id: data.id,
+                data: {
+                    name: data.name,
+                    address: data.address,
+                    client_id: data.client.id,
+                    notifications: data.notifications,
+                    status: data.status,
+                },
                 createMode: false,
             });
         }
+        this.loadClients();
     }
 
     onChange(event) {
@@ -43,10 +65,31 @@ class ConstructionForm extends Component {
         });
     }
 
+    async loadClients() {
+        this.setState({
+            loading: true,
+        });
+
+        await getClients()
+            .then((response) => {
+                this.setState({
+                    clients: response.data,
+                    loading: false,
+                });
+            }).catch((error) => {
+                if (error.response.status === 403 || error.response.status === 401) {
+                    const [, dispatch] = this.context;
+                    dispatch({
+                        type: 'EXIT',
+                    });
+                }
+            });
+    }
+
     validForm() {
-        const { data } = this.state;
+        const { data, createMode } = this.state;
         const {
-            name, business_name, rut, contact, phone, email, address,
+            name, address, client_id, notifications, status,
         } = data;
         const errors = {};
         let formIsValid = true;
@@ -56,34 +99,26 @@ class ConstructionForm extends Component {
             errors.name = 'Requerido';
         }
 
-        if (!business_name || business_name.trim().length === 0) {
-            formIsValid = false;
-            errors.business_name = 'Requerido';
-        }
-
-        if (!rut || rut.trim().length === 0) {
-            formIsValid = false;
-            errors.rut = 'Requerido';
-        }
-
-        if (!contact || contact.trim().length === 0) {
-            formIsValid = false;
-            errors.contact = 'Requerido';
-        }
-
-        if (!phone || contact.trim().length === 0) {
-            formIsValid = false;
-            errors.phone = 'Requerido';
-        }
-
-        if (!email || email.trim().length === 0) {
-            formIsValid = false;
-            errors.email = 'Requerido';
-        }
-
         if (!address || address.trim().length === 0) {
             formIsValid = false;
             errors.address = 'Requerido';
+        }
+
+        if (!client_id || client_id === 0) {
+            formIsValid = false;
+            errors.client_id = 'Requerido';
+        }
+
+        if (!notifications || notifications === 0) {
+            formIsValid = false;
+            errors.notifications = 'Requerido';
+        }
+
+        if (!createMode) {
+            if (!status || status === 0) {
+                formIsValid = false;
+                errors.status = 'Requerido';
+            }
         }
 
         this.setState({
@@ -116,9 +151,9 @@ class ConstructionForm extends Component {
     async handleUpdate() {
         if (this.validForm()) {
             this.toggleLoading(true);
-            const { data } = this.state;
+            const { data, id } = this.state;
 
-            await updateConstruction(data).then((response) => {
+            await updateConstruction(data, id).then((response) => {
                 if (response && response.status === 200) {
                     const { callback } = this.props;
                     callback();
@@ -141,26 +176,29 @@ class ConstructionForm extends Component {
     render() {
         const { readOnly } = this.props;
         const {
-            createMode, errors, data, loading,
+            createMode, errors, data, loading, clients,
         } = this.state;
         const {
-            name, business_name, rut, contact, phone, email, address,
+            name, address, client_id, notifications, status,
         } = data;
 
         const rest = {
-            onChange: this.onChang,
+            onChange: this.onChange,
             errors,
             required: true,
             readOnly,
-        }
+        };
 
         return (
             <>
                 <Row>
                     <Col md={12}><Input name="name" label="Nombre" value={name} icon="fas fa-industry" {...rest} /></Col>
                     <Col md={12}><Input name="address" label="Dirección" value={address} icon="fas fa-map-marked-alt" {...rest} /></Col>
-                    <Col md={12}><Select name="cliente_id" label="Cliente" value={business_name} {...rest} options={[{}]} placeholder="Seleccione..." /></Col>
-                    <Col md={12}><Input name="email" label="Emails" placeholder="correo1@ejemplo.com" value={email} icon="far fa-envelope" {...rest} /></Col>
+                    <Col md={12}><Select name="client_id" label="Cliente" value={client_id} options={clients} placeholder="Seleccione..." {...rest} /></Col>
+                    {!createMode && (
+                        <Col md={12}><Select name="status" label="Estado" value={status} options={status_options} placeholder="Seleccione..." {...rest} /></Col>
+                    )}
+                    <Col md={12}><Input name="notifications" label="Emails" placeholder="correo1@ejemplo.com" value={notifications} icon="far fa-envelope" {...rest} /></Col>
                     <Col md={12}>* Estas direcciones de correo recibirán reporte y estado de las maquinarias cuando esta salgan del taller hacia la obra del cliente</Col>
                 </Row>
                 {!readOnly && (
