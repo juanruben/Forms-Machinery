0796b02105289638e1d24c194fea02639f1060e9
juanruben <juanruben@gmail.com>
2019-11-26 17:01:29 -0300

    Renombra componente

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -14,7 +14,7 @@ import {
 
 import './Register.scss';
 
-class CheckIn extends Component {
+class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -247,4 +247,4 @@ class CheckIn extends Component {
     }
 }
 
-export default CheckIn;
+export default Register;
