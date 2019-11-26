35e61a74d126ddb4ff082d71138f746cc5f1e991
juanruben <juanruben@gmail.com>
2019-11-26 11:45:31 -0300

    Refactorización general

--- src/Pages/CheckIn/CheckIn.jsx
+++ src/Pages/CheckIn/CheckIn.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { getClients, getMachines, getConstructionsByClient, getForm } from "../../Service/Api";
 import { Row, Col } from 'reactstrap';
 import Title from '../../Components/Title/Title';
 import Select from '../../Components/Select/Select';
@@ -8,6 +7,9 @@ import Simple from '../../Components/Simple/Simple';
 import Multiple from '../../Components/Multiple/Multiple';
 import Input from '../../Components/Input/Input';
 import Photo from '../../Components/Photo/Photo';
+import {
+    getClients, getMachines, getConstructionsByClient, getForm,
+} from '../../Service/Api';
 
 import './CheckIn.scss';
 
@@ -22,21 +24,17 @@ class CheckIn extends Component {
             form: {
                 model_section: [
                     {
-                        model_field: []
-                    }
-                ]
+                        model_field: [],
+                    },
+                ],
             },
             errors: {},
-            client: {
-                id: 0
-            },
-            machine: {
-                id: 0
-            },
-            construction: {}
         };
+        this.onChange = this.onChange.bind(this);
         this.onChangeClient = this.onChangeClient.bind(this);
         this.onChangeMachine = this.onChangeMachine.bind(this);
+        this.handleSend = this.handleSend.bind(this);
+        this.validForm = this.validForm.bind(this);
     }
 
     componentDidMount() {
@@ -44,137 +42,154 @@ class CheckIn extends Component {
         this.loadMachines();
     }
 
-    //Loaders
-
-    async loadClients() {
-
-        await getClients().then((response) => {
-
-            let { clients } = this.state;
-            clients = response.data;
-            this.setState({
-                clients,
-            })
-        })
-
+    onChangeClient(event) {
+        const { value } = event.target;
+        this.onChange(event);
+        this.loadConstructionsByClientId(parseInt(value));
     }
 
-    async loadMachines() {
+    async onChangeMachine(event) {
+        const { value } = event.target;
+        const { machines } = this.state;
 
-        await getMachines().then((response) => {
+        this.onChange(event);
+        const machine = machines.find((item) => item.id === parseInt(value));
 
-            let { machines } = this.state;
-
-            machines = response.data;
-
+        await getForm(machine.model_form_id).then((response) => {
             this.setState({
-                machines,
-            })
-        })
+                form: response.data,
+            });
+        });
+    }
 
+    onChange(event) {
+        const { name, value } = event.target;
+        const { data, errors } = this.state;
+        data[name] = value;
+        errors[name] = '';
+        this.setState({
+            data,
+            errors,
+        });
     }
 
     async loadConstructionsByClientId(id) {
         await getConstructionsByClient(id)
             .then((response) => {
-                console.log(response)
-                let { constructions } = this.state;
-                constructions = response.data.construction;
                 this.setState({
-                    constructions,
+                    constructions: response.data.construction,
                 });
-            })
+            });
     }
 
-
-    //On Changes Events
-
-    onChangeClient(event) {
-        const { value } = event.target;
-        const { clients } = this.state;
-
-        let client = clients.find((item) => {
-            return item.id === parseInt(value)
-        });
-
-        this.loadConstructionsByClientId(client.id);
-
-        this.setState({
-            client,
-        });
-    }
-
-    async onChangeMachine(event) {
-
-        const { value } = event.target;
-        const { machines } = this.state;
-
-        let machine = machines.find((item) => {
-            return item.id === parseInt(value)
-        });
-
-        await getForm(machine.model_form_id).then((response) => {
-
+    async loadClients() {
+        await getClients().then((response) => {
             this.setState({
-                form: response.data
-            })
-        })
+                clients: response.data,
+            });
+        });
+    }
+
+    async loadMachines() {
+        await getMachines().then((response) => {
+            this.setState({
+                machines: response.data,
+            });
+        });
+    }
+
+    validForm() {
+        const { data } = this.state;
+        const { client, construction, machine } = data;
+        const errors = {};
+        let formIsValid = true;
+
+        if (!client) {
+            formIsValid = false;
+            errors.client = ['Requerido'];
+        }
+
+        if (!construction) {
+            formIsValid = false;
+            errors.construction = ['Requerido'];
+        }
+
+        if (!machine) {
+            formIsValid = false;
+            errors.machine = ['Requerido'];
+        }
 
         this.setState({
-            machine,
+            errors,
         });
+
+        return formIsValid;
+    }
+
+    handleSend() {
+        if (this.validForm()) {
+            alert("ok");
+        }
     }
 
     render() {
-        const { errors, clients, machines, constructions, client, machine, form } = this.state;
-
-        // TODO. Pasar todo array de errors
+        const {
+            errors, clients, machines, constructions, form, data,
+        } = this.state;
+        const { client, machine, construction } = data;
 
         return (
-
             <div className="check-in-container">
                 <Title text="Datos generales" />
 
                 <div className="check-in-container__section">
                     <Row>
-                        <Col md={6}><Select required label="Cliente" options={clients} placeholder="Seleccione..." name="client" onChange={this.onChangeClient} value={client.id.toString()} errors={errors} /></Col>
-                        <Col md={6}><Select label="Obra" options={constructions} placeholder="Seleccione..." name="name" onChange={() => { }} /></Col>
-                        <Col md={12}><Select label="Código de máquina" options={machines} placeholder="Seleccione..." name="machine" value={machine.id.toString()} onChange={this.onChangeMachine} /></Col>
+                        <Col md={6}><Select name="client" required label="Cliente" options={clients} placeholder="Seleccione..." onChange={this.onChangeClient} value={client} errors={errors} /></Col>
+                        <Col md={6}><Select name="construction" label="Obra" options={constructions} placeholder="Seleccione..." onChange={this.onChange} value={construction} errors={errors} /></Col>
+                        <Col md={6}><Select name="machine" label="Código de máquina" options={machines} placeholder="Seleccione..." value={machine} onChange={this.onChangeMachine} errors={errors} /></Col>
                     </Row>
-                    <Row>
-                        <Col md={12}>
-                            <Title text={form.name ? form.name : ""} />
-                        </Col>
-                    </Row>
-                        {form.model_section.map((section) => {
-                            return (
-                                <>                                    
-                                    <Row key={"section" + section.id}>
-                                    
-                                        <Col md={12} style={{marginLeft: "20px"}}>
-                                            <Title text={section.name ? section.name : ""} />
-                                        </Col> 
-                                        
-                                        {section.model_field.map( (field) => {
-                                            return (                                            
-                                                (field.type === 'multiple' ? 
-                                                    <Col md={6} key={"field" + field.id} style={{padding: "5px 30px"}}><Multiple required label={field.name} options={field.options} name={field.name} onChange={() => { }} errors={errors} /></Col>
-                                                    : field.type === 'image' ? 
-                                                        <Col md={6} key={"field" + field.id} style={{padding: "5px 30px"}}><Photo label={field.name} name="name3" onChange={() => { }} /></Col>
-                                                        : field.type === 'simple' ?
-                                                        <Col md={6} key={"field" + field.id} style={{padding: "5px 30px"}}><Simple label={field.name} name="name" onChange={() => { }} /></Col> 
-                                                            : <Col md={6} key={"field" + field.id} style={{padding: "5px 30px"}}><Input label={field.name} name="name" placeholder="Nombre" onChange={() => { }} /></Col>
-                                                )
-                                            )
-                                        })}                               
+                </div>
+                <Row>
+                    <Col md={12}>
+                        {form.name && <Title text={form.name} />}
+                    </Col>
+                </Row>
 
-                                    </Row>
-                                </>
-                            )                           
-                            
-                        })}
+                {form.model_section.map((section) => (
+                    <div className="check-in-container__section">
+                        <Row key={section.id}>
+                            <Col md={12}>
+                                {section.name && <Title text={section.name} />}
+                            </Col>
 
-                    {/* <Row>
+                            {section.model_field.map((field) => {
+                                switch (field.type) {
+                                    case 'multiple':
+                                        return (
+                                            <Col md={6} key={field.id} className="check-in-container__field"><Multiple required label={field.name} options={field.options} name={field.name} onChange={() => { }} errors={errors} /></Col>
+                                        );
+                                    case 'image':
+                                        return (
+                                            <Col md={6} key={field.id} className="check-in-container__field"><Photo label={field.name} name="name3" onChange={() => { }} /></Col>
+                                        );
+                                    case 'simple':
+                                        return (
+                                            <Col md={6} key={field.id} className="check-in-container__field"><Simple label={field.name} name="name" onChange={() => { }} /></Col>
+                                        );
+                                    case 'text':
+                                        return (
+                                            <Col md={6} key={field.id} className="check-in-container__field"><Input label={field.name} name="name" placeholder={field.name} onChange={() => { }} /></Col>
+                                        );
+                                    default:
+                                        return null;
+                                }
+                            })}
+
+                        </Row>
+                    </div>
+                ))}
+
+                {/* <Row>
                         <Col md={6}><Simple label="Esto es un ejemplo de selección simple" name="name" onChange={() => { }} /></Col>
                         <Col md={6}><Multiple required label="Esto es un ejemplo de múltiples opciones" options={options} name="test" onChange={() => { }} errors={errors} /></Col>
                         <Col md={6}><Multiple label="Otro ejemplo de múltiples opciones" options={options2} name="name2" onChange={() => { }} /></Col>
@@ -183,11 +198,11 @@ class CheckIn extends Component {
                         <Col md={4}><Photo label="Toma otra foto de la máquina" name="name2" onChange={() => { }} /></Col>
                         <Col md={4}><Photo label="Y otra foto más" name="name3" onChange={() => { }} /></Col>
                     </Row> */}
-                </div>
+
                 <Row>
                     <Col md={8} />
                     <Col md={4}>
-                        <Button text="Enviar" onClick={() => { }} />
+                        <Button text="Enviar" onClick={this.handleSend} />
                     </Col>
                 </Row>
             </div>
