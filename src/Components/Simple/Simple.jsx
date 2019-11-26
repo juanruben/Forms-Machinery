24de31764168d84ff2791daa57b89fb9de555ccf
juanruben <juanruben@gmail.com>
2019-11-26 16:30:16 -0300

    Gestiona parámetros value y required

--- src/Components/Simple/Simple.jsx
+++ src/Components/Simple/Simple.jsx
@@ -3,12 +3,17 @@ import PropTypes from 'prop-types';
 import './Simple.scss';
 
 function Simple(props) {
-    const { label, name, onChange } = props;
+    const {
+        label, name, onChange, value, required,
+    } = props;
     return (
         <div className="simple-container">
-            <div>{label}</div>
+            <div>
+                {label}
+                {required ? ' *' : ''}
+            </div>
             <label className="switch" htmlFor={name}>
-                <input id={name} name={name} type="checkbox" onChange={onChange} />
+                <input id={name} name={name} type="checkbox" onChange={onChange} checked={value} />
                 <span className="slider round" />
             </label>
         </div>
@@ -19,6 +24,13 @@ Simple.propTypes = {
     label: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
+    value: PropTypes.bool,
+    required: PropTypes.bool,
+};
+
+Simple.defaultProps = {
+    value: false,
+    required: false,
 };
 
 export default Simple;
