455555a837ee23f38c68aec955e7e393aadce826
juanruben <juanruben@gmail.com>
2019-12-20 14:18:59 -0300

    Ordena alfabeticamente selects de clientes y obras

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -300,6 +300,14 @@ class Register extends Component {
         return machines.filter((machine) => machine.status === 'En obra');
     }
 
+    getClientsForSelect = (clients) => {
+        return clients.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
+    getConstructionsForSelect = (constructions) => {
+        return constructions.sort((a, b) => a.name.localeCompare(b.name));
+    }
+
     getMachinesForSelect = (machines) => {
         const list = [];
         for (let ii = 0; ii < machines.length; ii += 1) {
@@ -547,9 +555,9 @@ class Register extends Component {
 
                 <div className="check-in-container__section">
                     <Row>
-                        <Col md={6}><Select name="client" required label="Cliente" options={clients} placeholder="Seleccione..." onChange={this.onChangeClient} value={client} errors={errors} loading={loadingClients} /></Col>
+                        <Col md={6}><Select name="client" required label="Cliente" options={this.getClientsForSelect(clients)} placeholder="Seleccione..." onChange={this.onChangeClient} value={client} errors={errors} loading={loadingClients} /></Col>
                         <Col md={6}><Select name="machine" required label="Máquina" options={this.getMachinesForSelect(machines)} placeholder="Seleccione..." value={machine} onChange={this.onChangeMachine} errors={errors} loading={loadingMachines} /></Col>
-                        <Col md={6}><Select name="construction" required label="Obra" options={constructions} placeholder="Seleccione..." onChange={this.onChangeConstruction} value={construction} loading={loadingConstructions} errors={errors} /></Col>
+                        <Col md={6}><Select name="construction" required label="Obra" options={this.getConstructionsForSelect(constructions)} placeholder="Seleccione..." onChange={this.onChangeConstruction} value={construction} loading={loadingConstructions} errors={errors} /></Col>
                         {constructionSelected.notifications && (
                             <>
                                 <Col md={12}>
